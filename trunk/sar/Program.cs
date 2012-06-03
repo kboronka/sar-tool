8c320663634f0fad3ded7cc5cc17d1cf1daca0cc
kboronka@gmail.com <kboronka@gmail.com@e0181cce-766e-d700-03fe-165ee013faab>
2012-06-03 15:10:03 +0000

    improved robustness

--- trunk/sar/Program.cs
+++ trunk/sar/Program.cs
@@ -5,7 +5,7 @@
  */
 using System;
 using System.IO;
-
+using System.Collections.Generic;
 using SkylaLib.Tools;
 
 namespace SkylaLib.sar
@@ -17,25 +17,47 @@ namespace SkylaLib.sar
 			try
 			{
 				Console.WriteLine(AssemblyInfo.Name + "  v" + AssemblyInfo.Version + "  " + AssemblyInfo.Copyright);
+				Console.WriteLine();
 				
-				if (args.Length != 3)
+				if (args.Length == 0)
 				{
 					Usage();
 					return;
 				}
-				else
+				
+				string command = args[0].ToLower();
+
+				if (command[0] != '-')
 				{
-					string root = Directory.GetCurrentDirectory();
-					#if DEBUG
-					Console.WriteLine("args[0]=" + args[0]);
-					Console.WriteLine("args[1]=" + args[1]);
-					Console.WriteLine("args[2]=" + args[2]);
-					#endif
-					
-					foreach (string file in IO.SearchAndReplaceInFiles(root, args[0], args[1], args[2]))
-					{
-						Console.WriteLine(file.Replace(root, ""));
-					}
+					Usage();
+					return;
+				}
+				
+				command = command.Substring(1);
+
+				#if DEBUG
+				int i = 0;
+				foreach (string arg in args)
+				{
+					Console.WriteLine("args[" + (i++).ToString() + "]=" + arg);
+				}
+
+				Console.WriteLine("Command = " + command);
+				#endif
+				
+				switch (command)
+				{
+					case "replace":
+						SearchReplace(args);
+						break;
+					case "help":
+						Usage();
+						break;
+					default:
+						Console.WriteLine("Unknown command");
+						Console.WriteLine("");
+						Usage();
+						break;
 				}
 			}
 			catch (Exception ex)
@@ -50,8 +72,42 @@ namespace SkylaLib.sar
 		
 		public static void Usage()
 		{
-			Console.WriteLine("Usage: sar <file_search_pattern> <search_text> <replace_text>");
-			Console.WriteLine("Example: sar \"*.ie9\" \"bing\" \"google\"");
+			Console.WriteLine("Usage: sar -replace <file_search_pattern> <search_text> <replace_text>");
+			Console.WriteLine("sar -replace \"AssemblyInfo.cs\" \"0.0.0.0\" \"1.0.0.0\"");
+			#if DEBUG
+			Console.ReadKey();
+			#endif
+		}
+		
+		public static void SearchReplace(string[] args)
+		{
+			// sanity check
+			if (args.Length != 4)
+			{
+				Usage();
+				return;
+			}
+			
+			string filePattern = args[1];
+			string searchString = args[2];
+			string replaceString = args[3];
+			
+			
+			string root = Directory.GetCurrentDirectory();
+			List<string> changedFiles = IO.SearchAndReplaceInFiles(root, filePattern, searchString, replaceString);
+			
+			if (changedFiles.Count > 0)
+			{
+				Console.WriteLine("Replacments made in the following file" + ((changedFiles.Count > 1) ? "s" : ""));
+				foreach (string file in changedFiles)
+				{
+					Console.WriteLine(file.Replace(root, ""));
+				}
+			}
+			else
+			{
+				Console.WriteLine("search string was not found");
+			}
 		}
 	}
 }
