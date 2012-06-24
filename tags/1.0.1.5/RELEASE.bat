b9b670d8d686db5425cd3b903567506fae99fe63
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2012-06-24 15:43:50 +0000

    Tagging the 1.0.1.5 version release of the project

---
+++ tags/1.0.1.5/RELEASE.bat
@@ -0,0 +1,86 @@
+:: Development Enviorment
+::
+:: Microsoft.NET v2.0.50727					http://www.microsoft.com/download/en/details.aspx?id=19
+:: SharpDevelop v3.2.1.6466					http://sourceforge.net/projects/sharpdevelop/files/SharpDevelop%203.x/3.2/SharpDevelop_3.2.1.6466_Setup.msi/download
+:: TortoiseSVN 1.7.4(+command line tools)	https://sourceforge.net/projects/tortoisesvn/files/1.7.4/Application/
+:: 7zip										http://www.7-zip.org/download.html
+
+
+:: GoogleCode://code.google.com/p/sar-tool/downloads/list
+:: SourceForge: http://sourceforge.net/projects/sartool/files/
+
+@echo off
+pushd "%~dp0"
+set SOLUTION=sar.sln
+set BASEURL=https://sar-tool.googlecode.com/svn
+set CONFIG=Release
+
+:: Paths
+	set BITS=x86
+	if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set BITS=x64
+	if "%PROCESSOR_ARCHITEW6432%" == "AMD64" set BITS=x64
+
+	IF %BITS% == x86 (
+		echo OS is 32bit
+		set MAKENSIS="%PROGRAMFILES%\NSIS\makensis.exe" /V3
+		set MSBUILD="%WinDir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe"
+		set REPLACE="lib\sar\sar.exe"
+		set ZIP="%PROGRAMFILES%\7-Zip\7zG.exe" a -tzip
+	) ELSE (
+		echo OS is 64bit
+		set MAKENSIS="%PROGRAMFILES(X86)%\NSIS\makensis.exe" /V3
+		set MSBUILD="%WinDir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe"
+		set REPLACE="lib\sar\sar.exe"
+		set ZIP="%PROGRAMFILES%\7-Zip\7zG.exe" a -tzip
+	)
+
+:: Build Soultion
+	echo "VERSION.MAJOR.MINOR.BUILD".
+	set /p VERSION="> "
+
+	svn update
+	%REPLACE% -replace AssemblyInfo.cs "0.0.0.0" "%VERSION%"
+	%REPLACE% -replace %SOLUTION% "Format Version 10.00" "Format Version 9.00"
+	%REPLACE% -replace %SOLUTION% "Visual Studio 2008" "Visual Studio 2005"
+
+	echo building binaries
+	%MSBUILD% %SOLUTION% /p:Configuration=%CONFIG% /p:Platform="x86"
+	if errorlevel 1 goto BuildFailed
+
+:: Build Complete
+	copy sar\bin\%CONFIG%\*.exe release\*.exe
+	copy license.txt release\license.txt
+
+	copy sar\bin\%CONFIG%\sar.exe sar.exe
+	%ZIP% "sar %VERSION%.zip" sar.exe readme.txt license.txt
+	del sar.exe
+
+	%REPLACE% -replace AssemblyInfo.cs "%VERSION%" "0.0.0.0"
+	%REPLACE% -replace %SOLUTION% "Format Version 9.00" "Format Version 10.00"
+	%REPLACE% -replace %SOLUTION% "Visual Studio 2005" "Visual Studio 2008"
+	
+	svn commit -m "version %VERSION%"
+	echo svn copy %BASEURL%/trunk %BASEURL%/tags/%VERSION% -m "Tagging the %VERSION% version release of the project"
+	svn copy %BASEURL%/trunk %BASEURL%/tags/%VERSION% -m "Tagging the %VERSION% version release of the project"
+	
+	echo
+	echo
+	echo build completed, trunk has been tagged
+
+	popd
+	exit /b 0
+
+
+:: Build Failed
+	:BuildFailed
+	%REPLACE% -replace AssemblyInfo.cs "%VERSION%" "0.0.0.0"
+	%REPLACE% -replace %SOLUTION% "Format Version 9.00" "Format Version 10.00"
+	%REPLACE% -replace %SOLUTION% "Visual Studio 2005" "Visual Studio 2008"
+
+	echo
+	echo
+	echo build failed
+	pause
+
+	popd
+	exit /b 1
---
+++ tags/1.0.1.5/sar/Program.cs
@@ -0,0 +1,192 @@
+﻿/*
+ * User: Kevin Boronka (kboronka@gmail.com)
+ * Date: 2/7/2012
+ * Time: 11:57 PM
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using SkylaLib.Tools;
+
+namespace SkylaLib.sar
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine(AssemblyInfo.Name + "  v" + AssemblyInfo.Version + "  " + AssemblyInfo.Copyright);
+				Console.WriteLine();
+				
+				if (args.Length == 0)
+				{
+					Usage();
+					return;
+				}
+				
+				string command = args[0].ToLower();
+
+				if (command[0] != '-')
+				{
+					Usage();
+					return;
+				}
+				
+				command = command.Substring(1);
+
+				#if DEBUG
+				int i = 0;
+				foreach (string arg in args)
+				{
+					Console.WriteLine("args[" + (i++).ToString() + "]=" + arg);
+				}
+
+				Console.WriteLine("Command = " + command);
+				#endif
+				
+				switch (command)
+				{
+					case "replace":
+					case "r":
+						SearchReplace(args);
+						break;
+					case "lv_ver":
+						LabVIEW_Version(args);
+						break;
+					case "help":
+						Usage();
+						break;
+					default:
+						Console.WriteLine("Unknown command");
+						Console.WriteLine("");
+						Usage();
+						break;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			
+			#if DEBUG
+			Console.ReadKey();
+			#endif
+		}
+		
+		public static void Usage()
+		{
+			Console.WriteLine("Usage:");
+			Console.WriteLine("\t -replace <file_search_pattern> <search_text> <replace_text>");
+			Console.WriteLine("\t -lv_ver <lvproj_file> <version>");
+			Console.WriteLine("Examples:");
+			Console.WriteLine("\t sar -replace \"AssemblyInfo.cs\" \"0.0.0.0\" \"1.0.0.0\"");
+			Console.WriteLine("\t sar -lv_ver \"*.lvproj_file\" \"1.0.2.1\"");
+			
+			#if DEBUG
+			string content = "<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">9</Property>";
+			string search = "<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">*</Property>";
+			string replace = "<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">1</Property>";
+			string newcontent = Regex.Replace(content, IO.WildcardToRegex(search), replace);
+			Console.WriteLine("content = " + content);
+			Console.WriteLine("search = " + search);
+			Console.WriteLine("replace " + replace);
+			Console.WriteLine("Result: ");
+			Console.WriteLine("\t" + Regex.Replace(content, IO.WildcardToRegex(search), replace));
+			
+			Console.ReadKey();
+			#endif
+		}
+		
+		public static void SearchReplace(string[] args)
+		{
+			// sanity check
+			if (args.Length != 4)
+			{
+				Usage();
+				return;
+			}
+			
+			string filePattern = args[1];
+			string searchString = args[2];
+			string replaceString = args[3];
+			
+			
+			string root = Directory.GetCurrentDirectory();
+			List<string> changedFiles = IO.SearchAndReplaceInFiles(root, filePattern, searchString, replaceString);
+			
+			if (changedFiles.Count > 0)
+			{
+				Console.WriteLine("Replacments made in the following file" + ((changedFiles.Count > 1) ? "s" : ""));
+				foreach (string file in changedFiles)
+				{
+					Console.WriteLine(file.Replace(root, ""));
+				}
+			}
+			else
+			{
+				Console.WriteLine("search string was not found");
+			}
+		}
+		
+		public static void LabVIEW_Version(string[] args)
+		{
+			// sanity check
+			if (args.Length != 3)
+			{
+				Usage();
+				return;
+			}
+			
+			string filePattern = args[1];
+			string[] version = args[2].Split('.');
+			
+			if (version.Length != 4)
+			{
+				Usage();
+				return;
+			}
+			
+			string root = Directory.GetCurrentDirectory();
+			
+			/*
+			<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">9</Property>
+			<Property Name=\"TgtF_fileVersion.major\" Type=\"Int\">9</Property>
+			<Property Name=\"TgtF_fileVersion.minor\" Type=\"Int\">9</Property>
+			<Property Name=\"TgtF_fileVersion.patch\" Type=\"Int\">9</Property>
+			 */
+			
+			List<string> changedFiles = new List<string>();
+			changedFiles.AddRange(IO.SearchAndReplaceInFiles(root, filePattern, "<Property Name=\"TgtF_fileVersion.major\" Type=\"Int\">*</Property>", "<Property Name=\"TgtF_fileVersion.major\" Type=\"Int\">" + version[0] + "</Property>"));
+			changedFiles.AddRange(IO.SearchAndReplaceInFiles(root, filePattern, "<Property Name=\"TgtF_fileVersion.minor\" Type=\"Int\">*</Property>", "<Property Name=\"TgtF_fileVersion.minor\" Type=\"Int\">" + version[1] + "</Property>"));
+			changedFiles.AddRange(IO.SearchAndReplaceInFiles(root, filePattern, "<Property Name=\"TgtF_fileVersion.patch\" Type=\"Int\">*</Property>", "<Property Name=\"TgtF_fileVersion.patch\" Type=\"Int\">" + version[2] + "</Property>"));
+			changedFiles.AddRange(IO.SearchAndReplaceInFiles(root, filePattern, "<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">*</Property>", "<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">" + version[3] + "</Property>"));
+			
+			// remove duplicates
+			changedFiles.Sort();
+			Int32 index = 0;
+			while (index < changedFiles.Count - 1)
+			{
+				if (changedFiles[index] == changedFiles[index + 1])
+					changedFiles.RemoveAt(index);
+				else
+					index++;
+			}
+
+
+			if (changedFiles.Count > 0)
+			{
+				Console.WriteLine("Replacments made in the following file" + ((changedFiles.Count > 1) ? "s" : ""));
+				foreach (string file in changedFiles)
+				{
+					Console.WriteLine(file.Replace(root, ""));
+				}
+			}
+			else
+			{
+				Console.WriteLine("search string was not found");
+			}
+		}
+	}
+}
