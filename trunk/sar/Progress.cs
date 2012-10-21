d58e75dda235cc1f1397207370ed56c986bb32fd
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2012-10-21 21:01:27 +0000

    added a process running indicator

---
+++ trunk/sar/Progress.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kboronka
+ * Date: 11/7/2012
+ * Time: 12:06 PM
+ */
+using System;
+using System.Threading;
+
+namespace skylib.sar
+{
+	public class Progress
+	{
+		public Progress()
+		{
+			
+		}
+		
+		public void DoWork()
+		{
+			char[] sequence = {'-', '\\', '|', '/'};
+			while(true)
+			{
+				for (int i = 0; i < 6; i++)
+				{
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					//Console.Write(sequence[i] + "\r");
+					Console.Write("running" + new String('.', i) + new String(' ', 6 - i) + "\r");
+					Console.ResetColor();
+					Thread.Sleep(100);
+				}
+			}			
+		}
+	}
+}
