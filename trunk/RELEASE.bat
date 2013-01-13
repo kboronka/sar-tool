3a47ee2a83b1865506dc6744be0ff4b5bfedf57f
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2013-01-13 15:00:22 +0000

    updated build script

--- trunk/RELEASE.bat
+++ trunk/RELEASE.bat
@@ -1,42 +1,41 @@
-:: Development Enviorment
-::
-:: Microsoft.NET v2.0.50727					http://www.microsoft.com/download/en/details.aspx?id=19
-:: SharpDevelop v3.2.1.6466					http://sourceforge.net/projects/sharpdevelop/files/SharpDevelop%203.x/3.2/SharpDevelop_3.2.1.6466_Setup.msi/download
-:: TortoiseSVN 1.7.4(+command line tools)	https://sourceforge.net/projects/tortoisesvn/files/1.7.4/Application/
-:: 7zip										http://www.7-zip.org/download.html
+:SoftwareRequired
+	:: Microsoft.NET v2.0						http://www.filehippo.com/download_dotnet_framework_2/
+	:: Microsoft.NET v3.5						http://www.filehippo.com/download_dotnet_framework_3/
+	:: TortoiseSVN (+command line tools)		http://www.filehippo.com/download_tortoisesvn/
+	:: 7zip 32bit								http://www.filehippo.com/download_7zip_32/
+	:: 7zip 64bit								http://www.filehippo.com/download_7-zip_64/
+	:: SharpDevelop v3.2.1.6466					http://sourceforge.net/projects/sharpdevelop/files/SharpDevelop%203.x/3.2/SharpDevelop_3.2.1.6466_Setup.msi/download
 
+:DownloadLink
+	:: GoogleCode://code.google.com/p/sar-tool/downloads/list
+	:: SourceForge: http://sourceforge.net/projects/sartool/files/
 
-:: GoogleCode://code.google.com/p/sar-tool/downloads/list
-:: SourceForge: http://sourceforge.net/projects/sartool/files/
-
-@echo off
-pushd "%~dp0"
-set SOLUTION=sar.sln
-set REPO=https://sar-tool.googlecode.com/svn
-set CONFIG=Release
-set BASEPATH=%~dp0
+:BuildEnvironment
+	@echo off
+	pushd "%~dp0"
+	set SOLUTION=sar.sln
+	set REPO=https://sar-tool.googlecode.com/svn
+	set CONFIG=Release
+	set BASEPATH=%~dp0
 
 :Paths
 	set SAR="lib\sar\sar.exe"
 	set ZIP="%PROGRAMFILES%\7-Zip\7zG.exe" a -tzip
 
+	
 :Build
 	echo "VERSION.MAJOR.MINOR.BUILD".
 	set /p VERSION="> "
 
-	svn update
-	
+	svn update	
 	%SAR% -r AssemblyInfo.* ((Version)\(\"\d+\.\d+\.\d+\.\d+\"\)) "Version(\"%VERSION%\")"
-	%SAR% -r %SOLUTION% "Format Version 10.00" "Format Version 9.00"
-	%SAR% -r %SOLUTION% "Visual Studio 2008" "Visual Studio 2005"
 
 	echo building binaries
-	%SAR% -b.net 2.0 %SOLUTION% /p:Configuration=%CONFIG% /p:Platform=\"x86\"
+	%SAR% -b.net 3.5 %SOLUTION% /p:Configuration=%CONFIG% /p:Platform=\"x86\"
 	if errorlevel 1 goto BuildFailed
 
-:BuildComplete
-	echo build completed
 	
+:BuildComplete
 	copy sar\bin\%CONFIG%\*.exe release\*.exe
 	copy license.txt release\license.txt
 	copy readme.txt release\readme.txt
@@ -44,13 +43,17 @@ set BASEPATH=%~dp0
 	copy sar\bin\%CONFIG%\sar.exe sar.exe
 	%ZIP% "sar %VERSION%.zip" sar.exe readme.txt license.txt
 	del sar.exe
-
-	%SAR% -r %SOLUTION% "Format Version 9.00" "Format Version 10.00"
-	%SAR% -r %SOLUTION% "Visual Studio 2005" "Visual Studio 2008"
 	
-	svn commit -m "version %VERSION%"
+	svn commit -m "sar version %VERSION%"
 	svn copy %REPO%/trunk %REPO%/tags/%VERSION% -m "Tagging the %VERSION% version release of the project"
 	svn update
+	
+	cd lib\skylib-source
+	svn commit -m "sar version %VERSION%"
+	svn update
+	cd BASEPATH
+
+	echo build completed
 	popd
 	exit /b 0
 
@@ -58,7 +61,5 @@ set BASEPATH=%~dp0
 :BuildFailed
 	echo build failed
 	pause
-	%SAR% -r %SOLUTION% "Format Version 9.00" "Format Version 10.00"
-	%SAR% -r %SOLUTION% "Visual Studio 2005" "Visual Studio 2008"
 	popd
 	exit /b 1	
