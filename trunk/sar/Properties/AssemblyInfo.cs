79ab2dbd618a744a4b3435b4e46ffbc80f8682aa
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2013-02-17 15:21:25 +0000

    sar version 1.0.1.22

--- trunk/sar/Properties/AssemblyInfo.cs
+++ trunk/sar/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-#region Using directives
+﻿#region Using directives
 
 using System;
 using System.Reflection;
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.21")]
+[assembly: AssemblyVersion("1.0.1.22")]
