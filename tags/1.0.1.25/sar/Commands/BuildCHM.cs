23c197909562a9ad3fcf20e233e6fa628076afe0
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2013-03-10 16:16:37 +0000

    Tagging the 1.0.1.25 version release of the project

---
+++ tags/1.0.1.25/sar/Commands/BuildCHM.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * User: kboronka
+ */
+
+using System;
+using skylib.Tools;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace skylib.sar
+{
+	public class BuildCHM : BaseCommand
+	{
+		public BuildCHM() : base("Build CHM help file",
+		                         new List<string> { "build.chm", "b.chm" },
+		                         "-b.chm <hhp_filepath>",
+		                         new List<string> { @"-b.chm help\help.hhp" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 2)
+			{
+				throw new ArgumentException("too few arguments");
+			}
+			
+			string hhpFile = args[1];
+			
+			// get list of hhc.exe file locations availble
+			List<String> files = IO.GetAllFiles(IO.ProgramFilesx86, "hhc.exe");
+			if (files.Count == 0)
+			{
+				files = IO.GetAllFiles(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ProgramFiles), "hhc.exe");
+			}
+			
+			// sanity - hhc.exe not installed
+			if (files.Count == 0)
+			{
+				throw new FileNotFoundException("sar unable to locate hhc.exe");
+			}
+			
+			string hhcPath = files[0];
+			
+			// sanity - solution file exists
+			if (!File.Exists(hhpFile))
+			{
+				throw new FileNotFoundException(hhpFile + " hhp file not found");
+			}
+			
+
+			string arguments = hhpFile;
+			
+			for (int i = 2; i < args.Length; i++)
+			{
+				arguments += " " + args[i];
+			}
+			
+			#if DEBUG
+			Console.WriteLine(hhpFile + " " + arguments);
+			#endif
+
+			
+			Process compiler = new Process();
+			compiler.StartInfo.FileName = hhcPath;
+			compiler.StartInfo.Arguments = arguments;
+			compiler.StartInfo.UseShellExecute = false;
+			compiler.StartInfo.RedirectStandardOutput = true;
+			compiler.Start();
+			string output = compiler.StandardOutput.ReadToEnd();
+			compiler.WaitForExit();
+			
+			if (compiler.ExitCode != 1)
+			{
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+				Console.WriteLine("Build Failed");
+				Console.ResetColor();
+				Console.ForegroundColor = ConsoleColor.DarkCyan;
+				Console.WriteLine(output);
+				Console.ResetColor();
+				Console.WriteLine("exit code: " + compiler.ExitCode.ToString());
+				return Program.EXIT_ERROR;
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+				Console.WriteLine("Build Successfully Completed");
+				Console.ResetColor();
+				return Program.EXIT_OK;
+			}
+		}
+	}
+}
---
+++ tags/1.0.1.25/sar/Commands/Kill.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * User: kboronka
+ */
+
+using System;
+using skylib.Tools;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace skylib.sar
+{
+	public class Kill : BaseCommand
+	{
+		public Kill() : base("Kill Process",
+		                     new List<string> { "kill", "k" },
+		                     @"-kill <FilePath> <backup_location>",
+		                     new List<string> { "-backup backup.zip \"c:\\backups\\\"" })
+		{
+		}
+		
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 2)
+			{
+				throw new ArgumentException("wrong number of arguments");
+			}
+			
+			string processName = args[1];
+			
+			Process[] foundProcess = Process.GetProcessesByName(processName);
+			if (foundProcess.Length != 0)
+			{
+
+				foreach (Process process in foundProcess)
+				{
+					Console.ForegroundColor = ConsoleColor.DarkYellow;
+					Console.WriteLine(process.ProcessName + " killed");
+					Console.ResetColor();
+					process.Kill();
+				}
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+				Console.WriteLine(processName + " not running");
+				Console.ResetColor();
+			}
+			
+			return Program.EXIT_OK;
+		}
+	}
+}
