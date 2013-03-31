4b7caa4574abb9ce7fdbe3e80d1dea2605ff4ed8
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2013-03-31 15:33:02 +0000

    fixed: vbox.manage command

--- sar/Commands/VboxManage.cs
+++ sar/Commands/VboxManage.cs
@@ -38,44 +38,17 @@ namespace skylib.sar
 			{
 				throw new ArgumentException("too few arguments");
 			}
+					
+			string exePath = IO.FindApplication("VBoxManage.exe");
 			
-			string command = args[1];
-			
-			// get list of VBoxManage.exe file locations availble
-			List<String> files = IO.GetAllFiles(IO.ProgramFiles + @"\Oracle\VirtualBox", "VBoxManage.exe");
-			if (files.Count == 0)
-			{
-				files = IO.GetAllFiles(IO.ProgramFilesx86 + @"\Oracle\VirtualBox", "VBoxManage.exe");
-			}
-			
-			// sanity - vbox not installed
-			if (files.Count == 0)
-			{
-				throw new FileNotFoundException("sar unable to locate VBoxManage.exe");
-			}
-			
-			string vboxManagePath = files[0];
-			
-			// sanity - solution file exists
-			if (!File.Exists(command))
-			{
-				throw new FileNotFoundException(command + " nsis file not found");
-			}
-			
-
 			string arguments = "";
-			
-			for (int i = 2; i < args.Length; i++)
+			for (int i = 1; i < args.Length; i++)
 			{
 				arguments += " " + args[i];
 			}
-			
-			#if DEBUG
-			ConsoleHelper.WriteLine(vboxManagePath + " " + arguments);
-			#endif
 
 			string output;
-			int exitcode = ConsoleHelper.Shell(vboxManagePath, arguments, out output);
+			int exitcode = ConsoleHelper.Shell(exePath, arguments, out output);
 				
 			if (exitcode != 0)
 			{
