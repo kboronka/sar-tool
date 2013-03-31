514bc7f08501b9bbf5d31fe70a75e7081136b848
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2013-03-31 15:11:01 +0000

    Tagging the 1.0.1.38 version release of the project

---
+++ tags/1.0.1.38/sar/Commands/BuildCHM.cs
@@ -0,0 +1,72 @@
+/* Copyright (C) 2013 Kevin Boronka
+ * 
+ * software is distributed under the BSD license
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using skylib.Tools;
+using System.Collections.Generic;
+using System.IO;
+
+namespace skylib.sar
+{
+	public class BuildCHM : BaseCommand
+	{
+		public BuildCHM() : base("Build CHM help file",
+		                         new List<string> { "build.chm", "b.chm" },
+		                         "-b.chm <hhp_filepath>",
+		                         new List<string> { @"-b.chm help\help.hhp" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 2)
+			{
+				throw new ArgumentException("too few arguments");
+			}
+					
+			string scriptFile =  "\"" + IO.FindFile(args[1]) + "\"";
+			string exePath = IO.FindApplication("hhc.exe");
+			
+			string arguments = scriptFile;
+			
+			for (int i = 2; i < args.Length; i++)
+			{
+				arguments += " " + args[i];
+			}
+			
+			#if DEBUG
+			ConsoleHelper.WriteLine(exePath + " " + arguments);
+			#endif
+
+			string output;
+			int exitcode = ConsoleHelper.Shell(exePath, arguments, out output);
+			
+			if (exitcode != 1)
+			{
+				ConsoleHelper.WriteLine("Build Failed", ConsoleColor.DarkYellow);
+				ConsoleHelper.WriteLine(output, ConsoleColor.DarkCyan);
+				return Program.EXIT_ERROR;
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("Build Successfully Completed", ConsoleColor.DarkYellow);
+				return Program.EXIT_OK;
+			}
+		}
+	}
+}
---
+++ tags/1.0.1.38/sar/Program.cs
@@ -0,0 +1,153 @@
+/* Copyright (C) 2013 Kevin Boronka
+ * 
+ * software is distributed under the BSD license
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using skylib.Tools;
+
+
+// binary download: https://sar-tool.googlecode.com/svn/trunk/release/sar.exe
+// release download: https://sar-tool.googlecode.com/svn/tags/
+
+namespace skylib.sar
+{
+	public class Program
+	{
+		public const int EXIT_OK = 0;
+		public const int EXIT_ERROR = 1;
+		
+		public static bool NoWarning = false;
+		public static bool Debug = false;
+
+		public static int Main(string[] args)
+		{
+			#if DEBUG
+			Program.Debug = true;
+			#endif
+			
+			//FIXME: no error handling here
+			Progress progressBar = new Progress();
+			Thread backgroundThread = new Thread(new ThreadStart(progressBar.Enable));
+			
+			try
+			{
+				// load all command modules
+				List<BaseCommand> allCommands = new List<BaseCommand>() {
+					new Backup(),
+					new BuildCHM(),
+					new BuildNSIS(),
+					new BuildSLN(),
+					new Help(),
+					new Kill(),
+					new LabviewVersion(),
+					new SearchAndReplace(),
+					new TimestampFilename(),
+					new WindowsLogin(),
+					new VboxManage(),
+					new FileFind(),
+					new FileDestory(),
+					new WindowsRearm(),
+					new WindowsRestart(),
+					new FileBsdStamp(),
+					new Delay()
+				};
+
+				// process command line arguments
+				bool commandlineActive = false;
+				int exitCode = EXIT_OK;
+				
+				while (!commandlineActive)
+				{
+					try
+					{
+						if (args.Length == 0)
+						{
+							commandlineActive = false;
+							args = new string[1];
+							
+							Help.WriteTitle();
+							ConsoleHelper.Write("> ", ConsoleColor.White);
+							args = StringHelper.ParseString(ConsoleHelper.ReadLine(), " ");
+							
+							if (args.Length == 0)
+							{
+								throw new ArgumentException("too few arguments");
+							}
+						}
+						else
+						{
+							commandlineActive = true;
+						}
+						
+						
+						string command = args[0].ToLower();
+						if (command[0] == '-' || command[0] == '/')
+						{
+							command = command.Substring(1);
+						}
+						
+						// Execute Command
+						if (command != "exit")
+						{
+
+							backgroundThread = new Thread(new ThreadStart(progressBar.Enable));
+							backgroundThread.Name = "RunningIndicator";
+							backgroundThread.IsBackground = true;
+							backgroundThread.Start();
+							exitCode = CommandHub.Execute(command, args);
+							args = new string[0];
+							backgroundThread.Abort();
+
+						}
+					}
+					catch (Exception ex)
+					{
+						args = new string[0];
+						backgroundThread.Abort();
+						
+						ConsoleHelper.WriteException(ex);
+
+						if (Program.Debug && commandlineActive)
+						{
+							Thread.Sleep(2000);
+						}
+						
+						exitCode = EXIT_ERROR;
+					}
+				}
+				
+				return exitCode;
+			}
+			catch (Exception ex)
+			{
+				backgroundThread.Abort();
+				ConsoleHelper.WriteException(ex);
+				
+				if (Program.Debug)
+				{
+					ConsoleHelper.WriteLine("Press anykey to continue", ConsoleColor.Yellow);
+					ConsoleHelper.ReadKey();
+				}
+				
+				return EXIT_ERROR;
+			}
+		}
+	}
+}
