d21b521305d5c5adfda151c30a2e076436468479
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2013-04-07 16:27:05 +0000

    Tagging the 1.0.1.42 version release of the project

---
+++ tags/1.0.1.42/sar/Commands/WindowsLogin.cs
@@ -0,0 +1,67 @@
+/* Copyright (C) 2013 Kevin Boronka
+ * 
+ * software is distributed under the BSD license
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using skylib.Tools;
+using System.Collections.Generic;
+using System.IO;
+
+namespace skylib.sar
+{
+	public class WindowsLogin : BaseCommand
+	{
+		public WindowsLogin() : base("Windows Login", new List<string> { "windows.login", "win.login", "net.login", "n.login" },
+		                         "-windows.login <ip> <domain/username> <password>",
+		                         new List<string>() { @"-windows.login \\192.168.0.244\temp test testpw" })
+		{
+			
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 4)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			string serverAddres = args[1];
+			Progress.Message = "Logging into " + serverAddres;
+			
+			string uncPath = serverAddres;
+			if (uncPath.Substring(0,2) != @"\\") uncPath = @"\\" + uncPath;
+			
+			string userName = args[2];
+			string password = args[3];
+			
+			string output;
+			int exitcode;
+			
+			exitcode = ConsoleHelper.Shell("net", @"use " + uncPath + @" /DELETE", out output);
+			exitcode = ConsoleHelper.Shell("net", @"use " + uncPath + @" /USER:" + userName + " " + password + " /PERSISTENT:NO", out output);
+			
+			if (exitcode != 0)
+			{
+				ConsoleHelper.WriteLine("Login to " + serverAddres + " has failed", ConsoleColor.DarkYellow);
+				return Program.EXIT_ERROR;
+			}
+
+			ConsoleHelper.WriteLine("Login to " + serverAddres + " was successful", ConsoleColor.DarkYellow);
+			return Program.EXIT_OK;
+		}
+	}
+}
