3835dc77aaca5e5ced94059fc530b26633c5368d
kboronka@gmail.com <kboronka@gmail.com@e0181cce-766e-d700-03fe-165ee013faab>
2013-04-14 17:11:10 +0000

    Tagging the 1.0.1.52 version release of the project

---
+++ tags/1.0.1.52/sar/Commands/FileLock.cs
@@ -0,0 +1,66 @@
+/* Copyright (C) 2013 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using skylib.Tools;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace skylib.sar
+{
+	public class FileLock : BaseCommand
+	{
+		public FileLock() : base("File - Lock",
+		                         new List<string> { "file.lock", "f.lock" },
+		                         "-file.find [filepattern] <timeout>",
+		                         new List<string> { "-file.find \"*.exe\" 10000" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 3)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			Progress.Message = "Waiting for lock";
+			string filePattern = args[1];
+			
+			int timeout = 5 * 60 * 1000;
+			Int32.TryParse(args[2], out timeout);
+			
+			string root = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref root, ref filePattern);
+			List<string> files = new List<string>();
+
+			
+			if (!IO.WaitForFileSystem(root, timeout, true))
+			{
+				ConsoleHelper.WriteLine("File System Not Found", ConsoleColor.DarkYellow);
+				return Program.EXIT_ERROR;
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("File System Found", ConsoleColor.DarkYellow);
+			}
+			
+			return Program.EXIT_OK;
+		}
+	}
+}
