11e298ecf70c23d0c2f7e3ff3e418eda33c4f882
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2013-04-28 22:23:22 +0000

    sar version 1.0.1.71

--- trunk/sar/Commands/CodeReIndent.cs
+++ trunk/sar/Commands/CodeReIndent.cs
@@ -1,4 +1,18 @@
-﻿
+/* Copyright (C) 2013 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 using System;
 using skylib.Tools;
 using System.Collections.Generic;
@@ -36,7 +50,6 @@ namespace skylib.sar
 			int counter = 0;
 			foreach (string file in files)
 			{
-				
 				List<string> indent = new List<string>();
 				
 				switch (IO.GetFileExtension(file).ToLower())
@@ -108,17 +121,7 @@ namespace skylib.sar
 						}
 						
 						IO.WriteFileLines(file, newlines);
-						
-						/*
-						ignore: "Private", "Public", "Shared", "Declare"
-						
-						indent:
-							startwith: "Class", "Function", "Enum", "Sub", "Select Case", "Case", "Do While", "Try", "Catch"
-							endswith: "_", "Then", "Else"
 
-						outdent:
-							start with "Loop", "End If", "End Try", "End Select", "End Sub", "End Function", "End Enum"
-						 */
 						break;
 					default:
 						break;
