85f3b9d7e7fd5c68c05507a00ea89c12da6c55c1
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-02 03:26:23 +0000

    Tagging the 1.0.1.120 version release of the project

---
+++ tags/1.0.1.120/sar/Commands/BuildSLN.cs
@@ -0,0 +1,93 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using sar.Tools;
+using System.Collections.Generic;
+using System.IO;
+
+namespace sar.Tools
+{
+	public class BuildSLN : BaseCommand
+	{
+		public BuildSLN() : base("Build - .NET soultion",
+		                         new List<string> { "build.net", "b.net" },
+		                         "-b.net [.net version] [solution_path] [msbuild arguments]",
+		                         new List<string> { "-b.net 3.5 sar.sln /p:Configuration=Release /p:Platform=\"x86\"" })
+		{
+			
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 2)
+			{
+				throw new ArgumentException("too few arguments");
+			}
+
+			// get list of msbuild versions available
+			string netVersion = args[1];
+			Progress.Message = "Locating Installed .NET versions";
+			string msbuildPath = IO.FindDotNetFolder(netVersion) + @"\MSBuild.exe";
+
+			// sanity check
+			if (args.Length < 3)
+			{
+				throw new ArgumentException("too few arguments");
+			}
+			
+			Progress.Message = "Searching";
+			string filePattern = args[2];
+			string root = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref root, ref filePattern);
+			List<string> files = IO.GetAllFiles(root, filePattern);
+			
+			// sanity - no solution file found
+			if (files.Count == 0) throw new FileNotFoundException(filePattern + " solution file not found");
+			
+			string soultionPath = files[0];
+			string solutionFileName = IO.GetFilename(soultionPath);
+			
+			// sanity - solution file exists
+			if (!File.Exists(soultionPath)) throw new FileNotFoundException(soultionPath + " solution file not found");
+			
+
+			string arguments = "\"" + soultionPath + "\"";
+			
+			for (int i = 3; i < args.Length; i++)
+			{
+				arguments += " " + args[i];
+			}
+			
+			Progress.Message = "Building .NET Solution " + solutionFileName;
+			
+			string output;
+			int exitcode = ConsoleHelper.Run(msbuildPath, arguments, out output);
+			if (exitcode != 0)
+			{
+				ConsoleHelper.DebugWriteLine("exit code: " + exitcode.ToString());
+				ConsoleHelper.WriteLine(output, ConsoleColor.DarkCyan);
+				ConsoleHelper.WriteLine("Build Failed", ConsoleColor.DarkYellow);
+				return exitcode;
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("Build Successfully Completed", ConsoleColor.DarkYellow);
+				return Program.EXIT_OK;
+			}
+		}
+	}
+}
---
+++ tags/1.0.1.120/sar/Commands/FileSearchAndReplace.cs
@@ -0,0 +1,72 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using sar.Tools;
+using System.Collections.Generic;
+using System.IO;
+
+namespace sar.Tools
+{
+	public class FileSearchAndReplace : BaseCommand
+	{
+		public FileSearchAndReplace() : base("File - Search And Replace",
+		                                 new List<string> { "replace", "r" },
+		                                 "-replace <file_search_pattern> <search_text> <replace_text>",
+		                                 new List<string> {
+		                                 	"-r \"AssemblyInfo.cs\" \"0.0.0.0\" \"1.0.0.0\"",
+		                                 	"-r AssemblyInfo.* ((Version)\\(\\\"\\d+\\.\\d+\\.\\d+\\.\\d+\\\"\\)) \"Version(\\\"%VERSION%\\\")\"",
+		                                 	"-r \\sar\\\"AssemblyInfo.cs\" \"0.0.0.0\" \"1.0.0.0\"",
+		                                 })
+		{
+			
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 4)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			string search = args[2];
+			string replace = args[3];
+
+			string filePattern = args[1];
+			string root = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref root, ref filePattern);
+			List<string> files = IO.GetAllFiles(root, filePattern);
+
+			int counter = 0;
+			int changes = 0;
+			foreach (string file in files)
+			{
+				if (Program.IncludeSVN || !IO.IsSVN(file))
+				{
+					int found = IO.SearchAndReplaceInFile(file, search, replace);
+					if (found > 0)
+					{
+						changes += found;
+						counter++;
+					}
+				}
+			}			
+
+			ConsoleHelper.WriteLine(changes.ToString() + " replacment" + ((changes != 1) ? "s" : "") + " made in " + counter.ToString() + " file" + ((counter != 1) ? "s" : ""), ConsoleColor.DarkYellow);
+			return Program.EXIT_OK;
+		}
+	}
+}
