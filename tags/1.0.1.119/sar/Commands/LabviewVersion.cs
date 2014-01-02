e3d947a5556e5db30afdf62dde8950be65b9d330
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-02 02:29:21 +0000

    Tagging the 1.0.1.119 version release of the project

---
+++ tags/1.0.1.119/sar/Commands/LabviewVersion.cs
@@ -0,0 +1,89 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using sar.Tools;
+using System.Collections.Generic;
+using System.IO;
+
+namespace sar.Tools
+{
+	public class LabviewVersion : BaseCommand
+	{
+		public LabviewVersion() : base("Set LabVIEW project version number",
+		                               new List<string> { "lv_ver" },
+		                               "-lv_ver [lvproj_file] [version]",
+		                               new List<string> { "-lv_ver \"*.lvproj_file\" \"1.0.2.1\"" })
+		{
+			
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 3)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			string[] version = args[2].Split('.');
+			
+			if (version.Length != 4)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			Progress.Message = "Searching";
+			string filePattern = args[1];
+			string root = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref root, ref filePattern);
+			List<string> files = IO.GetAllFiles(root, filePattern);
+			
+			ConsoleHelper.DebugWriteLine("pattern: " + filePattern);
+			ConsoleHelper.DebugWriteLine("root: " + root);
+			if (files.Count == 0) throw new FileNotFoundException("unable to find any files that match pattern: \"" + filePattern + "\" in root: \"" + root + "\"");
+
+			
+			int changes = 0;
+			int counter = 0;
+			foreach (string file in files)
+			{
+				if (Program.IncludeSVN || !IO.IsSVN(file))
+				{
+					counter++;
+					changes += IO.SearchAndReplaceInFile(file, "<Property Name=\"TgtF_fileVersion.major\" Type=\"Int\">\\d{1,}</Property>", "<Property Name=\"TgtF_fileVersion.major\" Type=\"Int\">" + version[0] + "</Property>");
+					changes += IO.SearchAndReplaceInFile(file, "<Property Name=\"TgtF_fileVersion.major\" Type=\"Int\">\\d{1,}</Property>", "<Property Name=\"TgtF_fileVersion.major\" Type=\"Int\">" + version[0] + "</Property>");
+					changes += IO.SearchAndReplaceInFile(file, "<Property Name=\"TgtF_fileVersion.minor\" Type=\"Int\">\\d{1,}</Property>", "<Property Name=\"TgtF_fileVersion.minor\" Type=\"Int\">" + version[1] + "</Property>");
+					changes += IO.SearchAndReplaceInFile(file, "<Property Name=\"TgtF_fileVersion.patch\" Type=\"Int\">\\d{1,}</Property>", "<Property Name=\"TgtF_fileVersion.patch\" Type=\"Int\">" + version[2] + "</Property>");
+					changes += IO.SearchAndReplaceInFile(file, "<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">\\d{1,}</Property>", "<Property Name=\"TgtF_fileVersion.build\" Type=\"Int\">" + version[3] + "</Property>");
+				}
+			}
+			
+			// remove duplicates
+
+
+			if (counter > 0)
+			{
+				ConsoleHelper.WriteLine("LabVIEW project version number updated", ConsoleColor.DarkYellow);
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("LabVIEW project version number not updated", ConsoleColor.DarkYellow);
+			}
+			
+			return Program.EXIT_OK;
+		}
+	}
+}
---
+++ tags/1.0.1.119/sar/Commands/VboxManage.cs
@@ -0,0 +1,65 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using sar.Tools;
+using System.Collections.Generic;
+using System.IO;
+
+namespace sar.Tools
+{
+	public class VboxManage : BaseCommand
+	{
+		public VboxManage() : base("Vbox Manage Tool",
+		                           new List<string> { "vbox.manage", "vb.manage" },
+		                           @"-vbox.manage [arg1] [arg2] [arg3]",
+		                           new List<string> { "-vbox.manage modifyhd \"WinXP-disk1.vdi\" --resize 3000" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 2)
+			{
+				throw new ArgumentException("too few arguments");
+			}
+					
+			string exePath = IO.FindApplication("VBoxManage.exe");
+			
+			string arguments = "";
+			for (int i = 1; i < args.Length; i++)
+			{
+				arguments += " " + args[i];
+			}
+
+			string output;
+			int exitcode = ConsoleHelper.Run(exePath, arguments, out output);
+				
+			if (exitcode != 0)
+			{
+				ConsoleHelper.WriteLine("Command Failed", ConsoleColor.DarkYellow);
+				ConsoleHelper.WriteLine(output, ConsoleColor.DarkCyan);
+				ConsoleHelper.WriteLine("exit code: " + exitcode.ToString());
+				return exitcode;
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("Command Successfully Completed", ConsoleColor.DarkYellow);
+				return Program.EXIT_OK;
+			}
+		}
+	}
+}
