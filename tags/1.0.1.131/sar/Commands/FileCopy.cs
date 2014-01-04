c878cb962d1cc91e7b51f4c7599ba70257838e28
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-04 22:40:34 +0000

    Tagging the 1.0.1.131 version release of the project

---
+++ tags/1.0.1.131/sar/Commands/FileCopy.cs
@@ -0,0 +1,74 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using sar.Tools;
+using sar.Base;
+
+namespace sar.Commands
+{
+	public class FileCopy : Command
+	{
+		public FileCopy(Base.CommandHub parent) : base(parent, "File - Copy",
+		                           new List<string> { "file.copy", "f.c" },
+		                           @"-file.copy [root\filepattern] [destination] <speedlimt>",
+		                           new List<string> { "-file.copy \"*.*\" \"\\\\10.242.211.57\transfer\\x\\\"" })
+		{
+		}
+		
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 3 || args.Length > 4 )
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			Progress.Message = "searching";
+			string destinationRoot = args[2];
+			string root = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref destinationRoot);
+
+			string filePattern = args[1];
+			IO.CheckRootAndPattern(ref root, ref filePattern);
+			List<string> files = IO.GetAllFiles(root, filePattern);
+			
+			int counter = 0;
+			for (int i = 0; i < files.Count; i++)
+			{
+				counter++;
+				// FIXME: the percentage complete string is not working
+				Progress.Message = i.ToString() + " of " + files.Count.ToString() + " " + ((i / files.Count) * 100).ToString() + "% ";
+				string filename = IO.GetFilename(files[i]);
+				string destinationPath = IO.GetRoot(destinationRoot + files[i].Substring(root.Length));
+
+				string sourceFile = files[i];
+				string destinationFile = destinationPath + filename;
+				
+				long len = IO.FileSize(files[i]);
+				ConsoleHelper.WriteLine(filename + ": " + len.ToString());
+				//if (!Directory.Exists(StringHelper.TrimEnd(destinationPath))) Directory.CreateDirectory(StringHelper.TrimEnd(destinationPath));
+				//if (!File.Exists(destinationFile)) File.Copy(sourceFile, destinationFile);
+			}
+
+			ConsoleHelper.WriteLine(counter.ToString() + " File" + ((counter != 1) ? "s" : "") + " Copied", ConsoleColor.DarkYellow);
+			return ConsoleHelper.EXIT_OK;
+		}
+	}
+}
---
+++ tags/1.0.1.131/sarTesting/RemoteSocket.cs
@@ -0,0 +1,50 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Timers;
+using System.Threading;
+using System.Windows.Forms;
+
+using sar.Tools;
+using sar.Socket;
+
+namespace sar.Testing
+{
+	public partial class RemoteSocket : Form
+	{
+		private SocketClient client1;
+
+		public RemoteSocket()
+		{
+			InitializeComponent();
+
+			client1 = new SocketClient("10.240.14.8", 8111, Encoding.ASCII);
+			this.socketMemCacheList1.Client = client1;
+			this.socketClientControl1.Client = client1;
+			client1.SendData("ping");
+		}
+		
+		void Connect3Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
