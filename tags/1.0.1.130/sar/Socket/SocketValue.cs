5d906bfe6819c69fc70effd1a952402a41043588
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-04 21:38:24 +0000

    Tagging the 1.0.1.130 version release of the project

---
+++ tags/1.0.1.130/sar/Socket/SocketValue.cs
@@ -0,0 +1,130 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+using sar.Tools;
+
+namespace sar.Socket
+{
+	public class SocketValue
+	{
+		private string name;
+		private string data;
+		private long sourceID;
+		private DateTime timestamp;
+		private DateTime lastUpdate;
+		
+		public SocketValue(string name)
+		{
+			this.name = name;
+			this.data = "";
+			this.timestamp = DateTime.Now;
+			this.lastUpdate = DateTime.Now;
+		}
+		
+		#region properties
+		
+		public string Name
+		{
+			get { return this.name; }
+		}
+		
+		public string Data
+		{
+			set
+			{
+				this.lastUpdate = DateTime.Now;
+				
+				if(this.data != value)
+				{
+					this.data = value;
+					this.timestamp = DateTime.Now;
+					OnDataChange(this);
+				}
+			}
+			get { return this.data; }
+		}
+		
+		public DateTime Timestamp
+		{
+			set { this.timestamp = value; }
+			get { return this.timestamp; }
+		}
+		
+		public DateTime LastUpdate
+		{
+			get { return lastUpdate; }
+		}
+		
+		public long SourceID
+		{
+			set { this.sourceID = value; }
+			get { return this.sourceID; }
+		}
+		
+		#endregion
+		
+		#region events
+		
+		#region data changed
+		
+		public delegate void DataChangedHandler(SocketValue sv);
+		private DataChangedHandler dataChanged = null;
+		
+		public event DataChangedHandler DataChanged
+		{
+			add
+			{
+				this.dataChanged += value;
+			}
+			remove
+			{
+				this.dataChanged -= value;
+			}
+		}
+		
+		private void OnDataChange(SocketValue data)
+		{
+			try
+			{
+				DataChangedHandler handler;
+				if (null != (handler = (DataChangedHandler)this.dataChanged))
+				{
+					handler(data);
+				}
+			}
+			catch
+			{
+
+			}
+		}
+
+		#endregion
+
+		#endregion
+		
+        public override string ToString()
+        {
+            return this.data;
+        }
+	}
+}
