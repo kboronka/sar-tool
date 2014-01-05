eeb5289459116160face289c6087e13a2babb852
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-05 01:03:47 +0000

    Tagging the 1.0.1.133 version release of the project

---
+++ tags/1.0.1.133/sarTesting/Program.cs
@@ -0,0 +1,47 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Windows.Forms;
+
+using sar.Tools;
+
+namespace sar.Testing
+{
+	internal sealed class Program
+	{
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Menu());
+		}
+		
+		private static FileLogger errorlogger;
+		public static FileLogger ErrorLog
+		{
+			get
+			{
+				if (Program.errorlogger == null)
+				{
+					Program.errorlogger = new FileLogger("error.log");
+				}
+				
+				return Program.errorlogger;
+			}
+		}
+	}
+}
