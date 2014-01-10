a3efeecbd779d4dda6ce82b9576d99853e8c58b0
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-10 02:09:48 +0000

    Tagging the 1.0.1.139 version release of the project

---
+++ tags/1.0.1.139/sar/Commands/BuildCHM.cs
@@ -0,0 +1,71 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using sar.Tools;
+using sar.Base;
+
+namespace sar.Commands
+{
+	public class BuildCHM : Command
+	{
+		public BuildCHM(Base.CommandHub parent) : base(parent, "Build - CHM help file",
+		                         new List<string> { "build.chm", "b.chm" },
+		                         "-b.chm [hhp_filepath]",
+		                         new List<string> { @"-b.chm help\help.hhp" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 2)
+			{
+				throw new ArgumentException("too few arguments");
+			}
+			
+			Progress.Message = "Searching";
+			string filepath = IO.FindFile(args[1]);
+			string filename = IO.GetFilename(filepath);
+			string exePath = IO.FindApplication("hhc.exe");
+			
+			string arguments = "\"" + filepath + "\"";
+			
+			for (int i = 2; i < args.Length; i++)
+			{
+				arguments += " " + args[i];
+			}
+			
+			Progress.Message = "Building Online Help " + filename;
+			
+			string output;
+			int exitcode = ConsoleHelper.Run(exePath, arguments, out output);
+			if (exitcode != 1)
+			{
+				ConsoleHelper.WriteLine("Build Failed", ConsoleColor.DarkYellow);
+				ConsoleHelper.WriteLine(output, ConsoleColor.DarkCyan);
+				return ConsoleHelper.EXIT_ERROR;
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("Build Successfully Completed", ConsoleColor.DarkYellow);
+				return ConsoleHelper.EXIT_OK;
+			}
+		}
+	}
+}
