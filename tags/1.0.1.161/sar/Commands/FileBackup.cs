c94c417b62d4c5f41e4709d60e5bc698b0517ec6
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-17 02:55:59 +0000

    Tagging the 1.0.1.161 version release of the project

---
+++ tags/1.0.1.161/sar/Commands/FileBackup.cs
@@ -0,0 +1,94 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using sar.Tools;
+using sar.Base;
+
+namespace sar.Commands
+{
+	public class FileBackup : Command
+	{
+		public FileBackup(Base.CommandHub parent) : base(parent, "File - Backup",
+		                           new List<string> { "file.backup", "f.bk" },
+		                           @"-file.backup [filepath/pattern] [destination]",
+		                           new List<string> { "-file.backup backup.zip \"c:\\backups\\\"" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 3)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			Progress.Message = "Searching";
+			string filePattern = args[1];
+			string root = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref root, ref filePattern);
+			List<string> files = IO.GetAllFiles(root, filePattern);
+			
+			ConsoleHelper.DebugWriteLine("pattern: " + filePattern);
+			ConsoleHelper.DebugWriteLine("root: " + root);
+			if (files.Count == 0) throw new FileNotFoundException("unable to find any files that match pattern: \"" + filePattern + "\" in root: \"" + root + "\"");
+			
+			Progress.Message = "Locating Archive Folder";
+			string archivepath = args[2];
+			string archiveroot = Directory.GetCurrentDirectory();
+			ConsoleHelper.DebugWriteLine("args[2]: " + args[2]);
+			archivepath = IO.CheckPath(archiveroot, archivepath);
+			ConsoleHelper.DebugWriteLine("archivepath: " + archivepath);
+			ConsoleHelper.DebugWriteLine("archiveroot: " + archiveroot);
+			if (!Directory.Exists(archiveroot))	Directory.CreateDirectory(archiveroot);
+
+			int counter = 0;
+			foreach (string file in files)
+			{
+				if (!file.Contains(archivepath))
+				{
+					if (this.commandHub.IncludeSVN || !IO.IsSVN(file))
+					{
+						string fileRelativePath = StringHelper.TrimStart(file, root.Length);
+						string backupFile = archivepath + file.Substring(root.Length);
+						string backupRoot = IO.GetRoot(backupFile);
+
+						
+						Progress.Message = "Coping " + fileRelativePath;
+						counter++;
+						
+						try
+						{
+							if (!Directory.Exists(backupRoot)) Directory.CreateDirectory(backupRoot);
+							if (File.Exists(backupFile)) File.Delete(backupFile);
+							IO.CopyFile(file, backupFile);
+						}
+						catch (Exception ex)
+						{
+							ConsoleHelper.WriteLine(ex.Message, ConsoleColor.Red);
+						}
+					}
+				}
+			}
+			
+			ConsoleHelper.WriteLine(counter.ToString() + " File" + ((counter != 1) ? "s" : "") + " Copied", ConsoleColor.DarkYellow);
+			return ConsoleHelper.EXIT_OK;
+		}
+	}
+}
---
+++ tags/1.0.1.161/sar/Commands/SkyUpdaterAdd.cs
@@ -0,0 +1,72 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+
+using sar.Tools;
+using sar.Base;
+
+namespace sar.Commands
+{
+	public class SkyUpdaterAdd : Command
+	{
+		public SkyUpdaterAdd(Base.CommandHub commandHub): base(commandHub, "SkyUpdater - Add file",
+		                             new List<string> { "sky.add" },
+		                             "-sky.add [xml] [file] [url]",
+		                             new List<string> { @"-sky.add info.xml .\release\sar.exe https://sar-tool.googlecode.com/svn/trunk/release/sar.exe" })
+		{
+
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 4)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+
+			Progress.Message = "Searching";
+			string filePattern = args[2];
+			string root = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref root, ref filePattern);
+			List<string> files = IO.GetAllFiles(root, filePattern);
+			if (files.Count == 0) throw new FileNotFoundException("File " + filePattern + " not found");
+			
+			Progress.Message = "Locating XML";
+			string xmlFilePattern = args[1];
+			string xmlRoot = Directory.GetCurrentDirectory();
+			IO.CheckRootAndPattern(ref xmlRoot, ref xmlFilePattern);
+			string xmlFile = IO.FindFile(xmlRoot, xmlFilePattern);
+			if (!Directory.Exists(xmlRoot)) throw new DirectoryNotFoundException(xmlRoot + " does not exist");
+			if (!File.Exists(xmlFile)) throw new FileNotFoundException(xmlFile + " does not exist");
+			
+			Progress.Message = "Generating XML";
+			SkyUpdater updater = SkyUpdater.Open(xmlFile);
+			foreach (string file in files)
+			{
+				updater.AddFile(StringHelper.TrimStart(file, root.Length), args[3]);
+			}
+			
+			updater.Save(xmlFile);
+			
+			ConsoleHelper.WriteLine(IO.GetFilename(xmlFile) + " updated", ConsoleColor.DarkYellow);
+			return ConsoleHelper.EXIT_OK;
+		}
+	}
+}
