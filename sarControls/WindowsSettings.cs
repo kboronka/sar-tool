df6c6fa39669e1ae6595ed6c0feeacbbf34fa596
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-18 21:48:46 +0000

    added property to help manipulate the scrollbar size

---
+++ sarControls/WindowsSettings.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Win32;
+
+using sar.Tools;
+
+namespace sar.Controls
+{
+	public static class WindowsSettings
+	{
+		public static int ScrollBarSize
+		{
+			set
+			{
+				int size = value * -15;
+				
+				RegistryKey windowMetricsKey = Registry.LocalMachine.OpenSubKey(@"HKEY_CURRENT_USER\Control Panel\Desktop\WindowMetrics\", true);
+
+				if (windowMetricsKey == null) throw new KeyNotFoundException("WindowMetrics key was not found");
+
+				windowMetricsKey.SetValue("ScrollHeight", size, RegistryValueKind.String);
+				windowMetricsKey.SetValue("ScrollWidth", size, RegistryValueKind.String);
+				windowMetricsKey.Close();
+			}
+		}
+	}
+}
