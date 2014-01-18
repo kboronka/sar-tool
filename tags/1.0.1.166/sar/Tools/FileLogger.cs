2c8f97aeabad858ccf51e3be36ae5c9d3b76822e
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-01-18 21:28:42 +0000

    Tagging the 1.0.1.166 version release of the project

---
+++ tags/1.0.1.166/sar/Tools/FileLogger.cs
@@ -0,0 +1,170 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Timers;
+using System.Threading;
+
+namespace sar.Tools
+{
+	public class FileLogger
+	{
+		//TODO: delete log files older the N days.
+		private const string DATETIMESTAMP = "yyyy/MM/dd HH:mm:ss.fff";
+		private const string TIMESTAMP = "HH:mm:ss.fff";
+		private const string FILETIMESTAMP = "yyyy-MM-dd";
+		public const string ISO8601_TIMESTAMP = "yyyy-MM-ddTHH:mm:ssZ";
+		
+		private string root;
+		private string filename;
+		
+		private StreamWriter writer;
+		private DateTime today;
+		private bool logTime;
+		private bool printSeperator = true;
+		
+		#region properties
+		
+		public bool LogTime
+		{
+			get { return this.logTime; }
+			set { this.logTime = value; }
+		}
+		
+		#endregion
+		
+		public FileLogger(string filename, bool logTimstamp)
+		{
+			try
+			{
+				this.filename = filename;
+				this.logTime = logTimstamp;
+				
+				// no filename
+				if (String.IsNullOrEmpty(filename)) this.filename = AssemblyInfo.Name + ".log";
+				
+				// no file extension
+				if (this.filename.IndexOf('.') == -1) this.filename += ".log";
+
+				// root = C:\ProgramData\Company\Product
+				this.root = ApplicationInfo.CommonDataDirectory;
+				if (!Directory.Exists(this.root)) Directory.CreateDirectory(this.root);
+				
+				string path = this.root + DateTime.Today.ToString(FILETIMESTAMP) + "." + this.filename;
+				if (!File.Exists(path)) this.printSeperator = false;
+				
+				this.writer = new StreamWriter(path, true);
+				
+				this.fileFlush = new Thread(FlushLoop);
+				this.fileFlush.IsBackground = true;
+				this.fileFlush.Priority = ThreadPriority.Lowest;
+				this.fileFlush.Start();
+			}
+			catch
+			{
+				
+			}
+		}
+		
+		~FileLogger()
+		{
+			try
+			{
+				this.writer.WriteLine("disposing file logger");
+				this.writer.Flush();
+				this.writer.Close();
+				flushLoopShutdown = true;
+			}
+			catch
+			{
+				
+			}
+		}
+		
+		public void WriteLine(string text, DateTime timestamp)
+		{
+			lock (this.root)
+			{
+				if (this.writer == null) return;
+				
+				lock (this.writer)
+				{
+					if (this.today == null || (DateTime.Today != this.today))
+					{
+						this.writer.Flush();
+						this.writer.Close();
+						
+						string path = this.root + DateTime.Today.ToString(FILETIMESTAMP) + "." + this.filename;
+						if (!File.Exists(path)) this.printSeperator = false;
+						
+						this.writer = new StreamWriter(path, true);
+						this.today = DateTime.Today;
+					}
+					
+					if (this.printSeperator) this.writer.WriteLine(ConsoleHelper.HR);
+					if (this.logTime) text = timestamp.ToString(TIMESTAMP) + "\t" + text;
+					this.writer.WriteLine(text);
+					this.printSeperator = false;
+				}
+			}
+		}
+		
+		public void WriteLine(string text)
+		{
+			WriteLine(text, DateTime.Now);
+		}
+		
+		#region service
+		
+		#region flush
+		
+		private Thread fileFlush;
+		private bool flushLoopShutdown;
+		
+		private void FlushLoop()
+		{
+			Thread.Sleep(1000);
+			
+			while (!flushLoopShutdown)
+			{
+				try
+				{
+					this.FlushFile();
+					Thread.Sleep(500);
+				}
+				catch
+				{
+					Thread.Sleep(5000);
+				}
+			}
+		}
+
+		internal void FlushFile()
+		{
+			lock (this.writer)
+			{
+				this.writer.Flush();
+			}
+		}
+		
+		#endregion
+		
+		#endregion
+	}
+}
---
+++ tags/1.0.1.166/sarTesting/Menu.Designer.cs
@@ -0,0 +1,126 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace sar.Testing
+{
+	partial class Menu
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.LocalSocketButton = new System.Windows.Forms.Button();
+			this.RemoteSocketButton = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.booleanIndicator1 = new sar.Controls.BooleanIndicator();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// LocalSocketButton
+			// 
+			this.LocalSocketButton.Location = new System.Drawing.Point(13, 13);
+			this.LocalSocketButton.Name = "LocalSocketButton";
+			this.LocalSocketButton.Size = new System.Drawing.Size(113, 23);
+			this.LocalSocketButton.TabIndex = 0;
+			this.LocalSocketButton.Text = "Local Socket";
+			this.LocalSocketButton.UseVisualStyleBackColor = true;
+			this.LocalSocketButton.Click += new System.EventHandler(this.LocalSocketClick);
+			// 
+			// RemoteSocketButton
+			// 
+			this.RemoteSocketButton.Location = new System.Drawing.Point(13, 42);
+			this.RemoteSocketButton.Name = "RemoteSocketButton";
+			this.RemoteSocketButton.Size = new System.Drawing.Size(113, 23);
+			this.RemoteSocketButton.TabIndex = 1;
+			this.RemoteSocketButton.Text = "Remote Socket";
+			this.RemoteSocketButton.UseVisualStyleBackColor = true;
+			this.RemoteSocketButton.Click += new System.EventHandler(this.RemoteSocketClick);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(338, 27);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 2;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.IntegralHeight = false;
+			this.comboBox1.Location = new System.Drawing.Point(234, 53);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(224, 32);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// booleanIndicator1
+			// 
+			this.booleanIndicator1.Caption = "booleanIndicator";
+			this.booleanIndicator1.Font = new System.Drawing.Font("Arial", 9.75F);
+			this.booleanIndicator1.Location = new System.Drawing.Point(234, 130);
+			this.booleanIndicator1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.booleanIndicator1.MaximumSize = new System.Drawing.Size(500, 16);
+			this.booleanIndicator1.MinimumSize = new System.Drawing.Size(100, 16);
+			this.booleanIndicator1.Name = "booleanIndicator1";
+			this.booleanIndicator1.Size = new System.Drawing.Size(131, 16);
+			this.booleanIndicator1.Status = false;
+			this.booleanIndicator1.TabIndex = 4;
+			// 
+			// Menu
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(507, 235);
+			this.Controls.Add(this.booleanIndicator1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.RemoteSocketButton);
+			this.Controls.Add(this.LocalSocketButton);
+			this.Name = "Menu";
+			this.Text = "Menu";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private sar.Controls.BooleanIndicator booleanIndicator1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Button RemoteSocketButton;
+		private System.Windows.Forms.Button LocalSocketButton;
+	}
+}
