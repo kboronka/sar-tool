6253053f7b3ec0a41ff3c6935854ff514ac38a26
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-02-22 02:31:38 +0000

    Tagging the 1.0.1.181 version release of the project

---
+++ tags/1.0.1.181/sar/Tools/ActiveDirectory.cs
@@ -0,0 +1,52 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Security.Principal;
+
+namespace sar.Tools
+{
+	public static class ActiveDirectory
+	{
+		public static string CurrentUserName()
+		{
+			string username = WindowsIdentity.GetCurrent().Name;
+			if (username.LastIndexOf(@"\") != -1)
+			{
+				return username.Substring(username.LastIndexOf(@"\") + 1);
+			}
+			
+			return username;
+		}
+				
+		public static string GetLDAP(string username)
+		{
+			DirectorySearcher searcher = new DirectorySearcher();
+			searcher.SearchScope = SearchScope.Subtree;
+			searcher.PropertiesToLoad.Add("distinguishedName");
+			searcher.PageSize = 1;
+			searcher.ServerPageTimeLimit = TimeSpan.FromSeconds(2);
+			searcher.Filter = "(&(objectCategory=user)(sAMAccountName=" + username + "))";
+
+			SearchResult searchResult = searcher.FindOne();
+
+			if (searchResult == null) throw new Exception ("no result found for " + username);
+			return searchResult.Path;
+		}		
+	}
+}
