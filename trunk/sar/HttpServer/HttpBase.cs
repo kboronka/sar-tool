e0115a64b937e1275b7cc8b47799f236bfeaeb26
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-05-09 00:42:25 +0000

    use base.Program.Logging functions

--- trunk/sar/HttpServer/HttpBase.cs
+++ trunk/sar/HttpServer/HttpBase.cs
@@ -23,37 +23,6 @@ namespace sar.HttpServer
 	{
 		public HttpBase()
 		{
-		}
-		
-		#region logger
-		
-		FileLogger debugLog;
-		ErrorLogger errorLogger;
-		
-		public FileLogger DebugLog
-		{
-			get { return this.debugLog; }
-			private set { this.debugLog = value; }
-		}
-		
-		public ErrorLogger ErrorLog
-		{
-			get { return this.errorLogger; }
-			private set { this.errorLogger = value; }
-		}
-		
-		protected void Log(string line)
-		{
-			if (this.debugLog == null) return;
-			this.debugLog.WriteLine(this.ToString() + ": " + line);
-		}
-		
-		protected void Log(Exception ex)
-		{
-			if (this.errorLogger == null) return;
-			this.errorLogger.Write(ex);
-		}
-		
-		#endregion		
+		}	
 	}
 }
