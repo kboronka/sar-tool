47fc261fb77cdd3ba027165d49a21fc011f7d54d
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-05-12 00:45:45 +0000

    improved folder removal (allow wildcards folder names)

--- sar/Commands/FileRemoveDirectory.cs
+++ sar/Commands/FileRemoveDirectory.cs
@@ -25,9 +25,9 @@ namespace sar.Commands
 	public class FileRemoveDirectory : Command
 	{
 		public FileRemoveDirectory(Base.CommandHub parent) : base(parent, "File - Remove Directory",
-		                                    new List<string> { "file.removedirectory", "f.rd", "d.d" },
-		                                    "-f.d [filepattern]",
-		                                    new List<string> { "-f.rd \"C:\\Temp\"" })
+		                                                          new List<string> { "file.removedirectory", "f.rd", "d.d" },
+		                                                          "-f.d [filepattern]",
+		                                                          new List<string> { "-f.rd \"C:\\Temp\"" })
 		{
 		}
 		
@@ -42,31 +42,61 @@ namespace sar.Commands
 			
 			Progress.Message = "Searching";
 			string root = args[1];
-			List<string> directories = IO.GetAllDirectories(root);
+			string filePattern = root.Substring(root.LastIndexOf(@"\") + 1, root.Length - root.LastIndexOf(@"\") - 1);
+			root = root.Substring(0, root.LastIndexOf(@"\") + 1);
 			
 			if (!Directory.Exists(root)) throw new ApplicationException("Directory does not exist");
+
+			
+			// get a list of all folders that match name pattern
+			List<string> foundDirectories = new List<string>();
+			foundDirectories.AddRange(Directory.GetDirectories(root, filePattern));
+			
+			if (foundDirectories.Count == 0) throw new ApplicationException("\"" + filePattern + "\" Folders not found in \"" + root + "\"");
 			
 			
+			List<string> directories = new List<string>();
+			List<string> files = new List<string>();
+			
+			foreach (string directory in foundDirectories)
+			{
+				foreach (string subDirectory in IO.GetAllDirectories(directory))
+				{
+					ConsoleHelper.Write("found: " , ConsoleColor.Cyan);
+					ConsoleHelper.WriteLine(StringHelper.TrimStart(subDirectory, directory.Length));
+				}
+				
+				directories.AddRange(IO.GetAllDirectories(directory));
+				
+				foreach (string file in IO.GetAllFiles(directory))
+				{
+					files.AddRange(IO.GetAllFiles(directory));
+					ConsoleHelper.Write("found: ", ConsoleColor.Cyan);
+					ConsoleHelper.WriteLine(StringHelper.TrimStart(file, directory.Length));
+				}
+				
+				files.AddRange(IO.GetAllFiles(directory));
+			}
+
+
 			if (!this.commandHub.NoWarning)
 			{
-				foreach (string directory in directories)
-				{
-					ConsoleHelper.Write("found: ", ConsoleColor.Cyan);
-					ConsoleHelper.WriteLine(StringHelper.TrimStart(directory, root.Length));
-				}
-
-				ConsoleHelper.WriteLine(directories.Count.ToString() + " file" + ((directories.Count != 1) ? "s" : "") + " found");
+				ConsoleHelper.WriteLine(directories.Count.ToString() + " folders" + ((directories.Count != 1) ? "s" : "") + " found");
+				ConsoleHelper.WriteLine(files.Count.ToString() + " files" + ((directories.Count != 1) ? "s" : "") + " found");
 			}
 			
 			if (directories.Count > 0)
 			{
 				if (this.commandHub.NoWarning || ConsoleHelper.Confirm("Destroy " + directories.Count.ToString() + " director" + ((directories.Count != 1) ? "ies" : "y") + "? (y/n)"))
 				{
-					Progress.Message = "Destroying " + root;
+					Progress.Message = "Deleting " + root;
 					
 					try
 					{
-						Directory.Delete(root, true);
+						foreach (string directory in foundDirectories)
+						{
+							Directory.Delete(directory, true);
+						}
 					}
 					catch (Exception ex)
 					{
