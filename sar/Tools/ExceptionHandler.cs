0a361d57384f5890784564a717d24f6155e285ac
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-05-23 01:27:06 +0000

    simplified logging of exeptions's stacktrace

--- sar/Tools/ExceptionHandler.cs
+++ sar/Tools/ExceptionHandler.cs
@@ -15,6 +15,8 @@
 
 using System;
 using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace sar.Tools
@@ -27,7 +29,7 @@ namespace sar.Tools
 			this.Text = ex.GetType().ToString();
 			this.details.Text = ex.Message;
 			this.details.Text += Environment.NewLine;
-			this.details.Text += ex.StackTrace;
+			this.details.Text += GetStackTrace(ex);
 		}
 		
 		public static void Display(Exception ex)
@@ -45,6 +47,45 @@ namespace sar.Tools
 			}
 			
 			return ex;
-		}		
+		}
+		
+		public static string GetStackTrace(Exception ex)
+		{
+			string result = "";
+			
+			string stackTrace = ex.StackTrace;
+			string regex = @"(\s*)at\s((.?)*)\sin\s((.?)*):line\s([1-9]*)";
+			
+			
+			string[] lines = stackTrace.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+			
+			foreach (string line in lines)
+			{
+				MatchCollection regexResults = Regex.Matches(line, regex);
+				if (regexResults.Count == 1 && regexResults[0].Groups.Count == 7)
+				{
+					if (!String.IsNullOrEmpty(result)) result += Environment.NewLine;
+					string method = regexResults[0].Groups[2].Value;
+					string filepath = regexResults[0].Groups[4].Value;
+					string lineNumber = regexResults[0].Groups[6].Value;
+
+					string filename = IO.GetFilename(filepath);
+					
+					if (method.Contains("."))
+					{
+						method = method.Substring(method.LastIndexOf('.') + 1);
+					}
+					
+					if (method.Contains("("))
+					{
+						method = method.Substring(0, method.LastIndexOf('('));
+					}
+										
+					result += "\t" + method + "() in " + filename + ":line " + lineNumber;
+				}
+			}
+			
+			return result;
+		}
 	}
 }
