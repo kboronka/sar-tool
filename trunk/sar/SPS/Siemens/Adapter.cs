d5eb9948596807d17607d7b33fd2289b42c5a2be
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-07-29 01:11:30 +0000

    partially implemented http://tools.ietf.org/html/rfc905

--- trunk/sar/SPS/Siemens/Adapter.cs
+++ trunk/sar/SPS/Siemens/Adapter.cs
@@ -8,10 +8,12 @@ using System.Net.Sockets;
 
 using sar.Tools;
 
+// specifications: http://tools.ietf.org/html/rfc905
+
 namespace sar.SPS.Siemens
 {
 	public enum AddressArea : byte { P = 0x80, I = 0x81, Q = 0x82, M = 0x83, DB = 0x84, DI = 0x85, L = 0x86, VL = 0x87 };
-	public enum Action : byte { Read = 0x4, Write = 0x5 };
+	public enum Action : byte { Read = 0x4, Write = 0x5, ExchangePDU = 0xF0 };
 	public enum TransportType : byte { Bit = 0x1, Byte = 0x2, Word = 0x4 };
 	
 	public class Adapter
@@ -21,9 +23,10 @@ namespace sar.SPS.Siemens
 		private NetworkStream stream;
 		
 		private static readonly byte[] CONNECT_TO_ADAPTER = 	new byte[] { 0x11, 0xE0, 0x00, 0x00, 0x00, 0x01, 0x00, 0xC0, 0x01, 0x09, 0xC1, 0x02, 0x4B, 0x54, 0xC2, 0x02, 0x03, 0x02 };
-		private static readonly byte[] CONNECTED_TO_ADAPTER =	new byte[] { 0x11, 0xD0, 0x00, 0x01, 0x00, 0x00, 0x00, 0xC0, 0x01, 0x09, 0xC1, 0x02, 0x4B, 0x54, 0xC2, 0x02, 0x03, 0x02 };
-		private static readonly byte[] TPKT_CONNECTION =		new byte[] { 0x03, 0x00, 0x00, 0x16 };
-		
+		private static readonly byte[] CONNECTED_TO_ADAPTER =	new byte[] { 0x11, 0xD0, 0x00, 0x01, 0x00, 0x00, 0x00, 0xC0, 0x01, 0x09, 0xC1, 0x02, 0x4B, 0x54, 0xC2, 0x02, 0x03, 0x02 };		
+		private static readonly byte[] EXCHANGE_PDU_PARAMETER =	new byte[] { 0xF0, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0xF0 };
+		private static readonly byte[] TPKT =					new byte[] { 0x03, 0x00, 0x00, 0x1F };
+
 		public Adapter(string ipAddress)
 		{
 			this.ipAddress = ipAddress;
@@ -38,8 +41,9 @@ namespace sar.SPS.Siemens
 			this.stream = this.socket.GetStream();
 
 			// write data to socket
-			byte[] packetBytes = IO.Combine(TPKT_CONNECTION, CONNECT_TO_ADAPTER);
-			stream.Write(packetBytes, 0, packetBytes.Length);
+			byte[] message = IO.Combine(TPKT, CONNECT_TO_ADAPTER);
+			EncodeTPKTSize(ref message);
+			stream.Write(message, 0, message.Length);
 
 			// wait for responce
 			byte[] responce = new byte[22];
@@ -48,7 +52,7 @@ namespace sar.SPS.Siemens
 			return responce.SequenceEqual(CONNECTED_TO_ADAPTER);
 		}
 		
-		private byte[] ReadWriteMessage(Action action, AddressArea addressArea, UInt16 dataBlock, UInt32 startAddress)
+		private byte[] ReadWriteMessage(Action action, AddressArea addressArea, ushort dataBlock, uint startAddress, ushort length)
 		{
 			byte[] message = new byte[] {(byte)action, 0x1};
 			
@@ -57,16 +61,102 @@ namespace sar.SPS.Siemens
 			
 			// transport type
 			message = IO.Combine(message, new byte[]  { (byte)TransportType.Byte });
-			                     
+			
+			// transport type
+			message = IO.Combine(message, new byte[]  { (byte)TransportType.Byte });
+
+			// length (bytes)
+			message = IO.Combine(message, IO.Split(length));
+
+			// DB number
+			if (addressArea != AddressArea.DB) dataBlock = 0;
+			message = IO.Combine(message, IO.Split(dataBlock));
+
+			// start address
+			startAddress = startAddress * 8;
+			message = IO.Combine(message, IO.SubSet(IO.Split(startAddress), 1, 3));
+
+			return message;
+		}
 		
-		return message;
-	}
-	
-	private int ReadInt()
-	{
-		return 0;
-	}
-	/*
+		private byte[] EncodeTPDU(byte[] parameterCode, byte[] parameterValue)
+		{
+			if (parameterCode.Length < 8) throw new InvalidDataException("incorred parameter code size");
+			if (parameterValue.Length > 32) throw new InvalidDataException("parameter value too large");
+
+			Action action = (Action)parameterCode[0];
+			ushort sequenceNumber = 0;
+			byte writeSize = 0;
+			
+			switch (action)
+			{
+				case Action.ExchangePDU:
+					sequenceNumber = 0x0D;
+					break;
+				case Action.Write:
+					sequenceNumber = 0x2C6;
+					// Data Header
+					writeSize = Convert.ToByte(parameterValue.Length);
+					parameterValue = IO.Combine(new byte[] { 0x0, 0x4, 0x0, (byte)(writeSize * 8) }, parameterValue);
+					break;
+				case Action.Read:
+					sequenceNumber = 0x3301;
+					break;
+				default:
+					throw new InvalidOperationException("unknown action " + action.ToString());
+			}
+
+			byte[] message = TPKT;
+			
+			// iso 8073
+			message = IO.Combine(message, new byte[] { 0x2, 0xF0, 0x80 });
+			
+			// PDU Header (10 bytes)
+			//	constant = 0x32
+			//	type = 0x1			
+			//	unknown = 0x0
+			//	unknown = 0x0
+			message = IO.Combine(message, new byte[] { 0x32, 0x1, 0x0, 0x0 });
+			
+			// sequence number (2 bytes)
+			message = IO.Combine(message, IO.Split(sequenceNumber));
+
+			// parameter size (2 bytes)
+			message = IO.Combine(message, IO.Split(Convert.ToUInt16(parameterCode.Length)));			
+			
+			// parameter value size (2 bytes)
+			message = IO.Combine(message, IO.Split(Convert.ToUInt16(parameterValue.Length)));
+			
+			// parameter
+			message = IO.Combine(message, parameterCode);
+			message = IO.Combine(message, parameterValue);
+
+			EncodeTPKTSize(ref message);
+				
+			return message;
+		}
+		
+		private void EncodeTPKTSize(ref byte[] message)
+		{
+			// 0                   1                   2                   3
+			// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+			// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+			// |      vrsn     |    reserved   |          packet length        |
+			// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+			
+			if (message.Length > ushort.MaxValue) throw new IndexOutOfRangeException("tpkt size exceeded");
+			ushort messageSize = Convert.ToUInt16(message.Length);
+			
+			message[2] = IO.Split(messageSize)[0];
+			message[3] = IO.Split(messageSize)[1];			
+		}
+		
+		private int ReadInt()
+		{
+			return 0;
+		}
+
+		/*
 Trying to read 16 bytes from FW0.
 PDU header:
                             0:0x32,0x01,0x00,0x00,0x00,0x00,0x00,0x0E,0x00,0x00,
@@ -106,7 +196,7 @@ FD4: 0
 FD8: 0
 FD12: 0.000000
 Finished.
-	 *
-	 */
-}
+		 *
+		 */
+	}
 }
