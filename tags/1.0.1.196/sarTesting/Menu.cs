71c0271e8c2cbac0c34534f5996c844ba0d87d6d
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-07-29 02:55:50 +0000

    Tagging the 1.0.1.196 version release of the project

---
+++ tags/1.0.1.196/sarTesting/Menu.cs
@@ -0,0 +1,64 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using sar.SPS.Siemens;
+
+namespace sar.Testing
+{
+	public partial class Menu : Form
+	{
+		public Menu()
+		{
+			InitializeComponent();
+		}
+		
+		void LocalSocketClick(object sender, EventArgs e)
+		{
+			LocalSocket frm = new LocalSocket();
+			frm.ShowDialog(this);
+			frm.Dispose();
+			
+		}
+		
+		void RemoteSocketClick(object sender, EventArgs e)
+		{
+			RemoteSocket frm = new RemoteSocket();
+			frm.ShowDialog(this);
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			try
+			{		
+				throw new Exception("test");
+			}
+			catch (Exception ex)
+			{
+				Program.Log(ex);
+			}			
+		}
+		
+		void ConnectToSPSClick(object sender, EventArgs e)
+		{
+			// TODO: Implement ConnectToSPSClick
+			Adapter SiemensSPS = new Adapter("10.242.35.166");
+			
+		}
+	}
+}
