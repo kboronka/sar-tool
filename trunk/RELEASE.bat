ce69564e10b86e76b03a9f03994401f75cd96b41
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-07-29 00:51:25 +0000

    added php software reference

--- trunk/RELEASE.bat
+++ trunk/RELEASE.bat
@@ -1,10 +1,13 @@
-:SoftwareRequired
+:Required Software
 	:: Microsoft.NET v2.0						http://www.filehippo.com/download_dotnet_framework_2/
 	:: Microsoft.NET v3.5						http://www.filehippo.com/download_dotnet_framework_3/
 	:: TortoiseSVN (+command line tools)		http://www.filehippo.com/download_tortoisesvn/
 	:: 7zip 32bit								http://www.filehippo.com/download_7zip_32/
 	:: 7zip 64bit								http://www.filehippo.com/download_7-zip_64/
 	:: SharpDevelop v4x							http://www.icsharpcode.net/OpenSource/SD/Download/#SharpDevelop4x
+
+:Optional Software
+	:: php										http://windows.php.net/download/
 
 :DownloadLink
 	:: GoogleCode: https://code.google.com/p/sar-tool/downloads/list
