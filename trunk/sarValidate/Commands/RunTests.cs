dd61cace912ee2f0fa9a1727ca20a96a1ba31d7e
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-08-02 00:01:49 +0000

    adding sarValidate to the solution

---
+++ trunk/sarValidate/Commands/RunTests.cs
@@ -0,0 +1,51 @@
+﻿/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using sar.Base;
+using sar.Tools;
+
+namespace sarValidate.Commands
+{
+	public class RunTests : sar.Base.Command
+	{
+		public RunTests(sar.Base.CommandHub parent) : base(parent, "RunTests",
+		                                              new List<string> { "run", "r" },
+		                                              @"-r",
+		                                              new List<string> { "-r" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 1)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			Program.Log("RunTests");
+			Progress.Message = "Running Test #1";
+			Thread.Sleep(1000);
+			Progress.Message = "Running Test #2";			
+			Thread.Sleep(1000);
+			
+			return ConsoleHelper.EXIT_OK;
+		}
+	}
+}
