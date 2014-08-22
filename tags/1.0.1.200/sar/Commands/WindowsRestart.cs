bab1fe58e9948f6439ef1e6474941c480f180c74
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-08-22 01:45:19 +0000

    Tagging the 1.0.1.200 version release of the project

---
+++ tags/1.0.1.200/sar/Commands/WindowsRestart.cs
@@ -0,0 +1,97 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+using sar.Tools;
+using sar.Base;
+
+namespace sar.Commands
+{
+	public class WindowsRestart : Command
+	{
+		public WindowsRestart(Base.CommandHub parent) : base(parent, "Windows - Restart",
+		                               new List<string> { "windows.restart", "win.restart" },
+		                               @"-windows.restart [ip | computername] [domain/username] [password] <timeout (ms)>",
+		                               new List<string> { "-windows.restart 192.168.0.244 admin-username mypassword 35000" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 4 || args.Length > 5)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			string serverAddres = args[1];
+			Progress.Message = "Rebooting " + serverAddres;
+			
+			string uncPath = serverAddres;
+			if (uncPath.Substring(0,2) != @"\\") uncPath = @"\\" + uncPath;
+			
+			string userName = args[2];
+			string password = args[3];
+			
+			int timeout = 5 * 60 * 1000;
+			Int32.TryParse(args[4], out timeout);
+			
+			int exitcode;
+			
+			exitcode = ConsoleHelper.Run("net", @"use " + uncPath + @" /DELETE /y");
+			exitcode = ConsoleHelper.Run("net", @"use " + uncPath + @" /USER:" + userName + " " + password);
+			
+			if (exitcode != 0)
+			{
+				ConsoleHelper.WriteLine("Login Failed", ConsoleColor.DarkYellow);
+				return ConsoleHelper.EXIT_ERROR;
+			}
+			
+			exitcode = ConsoleHelper.Run("shutdown", @"/r /m " + uncPath + @" /t 0 /f");
+
+			if (exitcode != 0)
+			{
+				ConsoleHelper.WriteLine(serverAddres + " failed to restart", ConsoleColor.DarkYellow);
+				return ConsoleHelper.EXIT_ERROR;
+			}
+			
+			if (timeout != 0)
+			{
+				Progress.Message = "Shutting Down " + serverAddres;
+				if (!NetHelper.WaitForPing(serverAddres, timeout, false))
+				{
+					ConsoleHelper.WriteLine(serverAddres + " did not shutdown", ConsoleColor.DarkYellow);
+					return ConsoleHelper.EXIT_ERROR;
+				}
+				
+				Progress.Message = "Starting " + serverAddres;
+				
+				if (!NetHelper.WaitForPing(serverAddres, timeout, true))
+				{
+					ConsoleHelper.WriteLine(serverAddres + " did not start", ConsoleColor.DarkYellow);
+					return ConsoleHelper.EXIT_ERROR;
+				}
+			}
+			
+			ConsoleHelper.WriteLine(serverAddres + " reboot complete", ConsoleColor.DarkYellow);
+			return ConsoleHelper.EXIT_OK;
+		}
+	}
+}
---
+++ tags/1.0.1.200/sar/Tools/XmlHelper.cs
@@ -0,0 +1,355 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Security;
+using System.Xml;
+
+namespace sar.Tools
+{
+	public class XML
+	{
+		public const string DATE = "yyyy-MM-dd";
+		public const string DATETIME = "yyyy-MM-dd HH:mm:ss";
+		public const string DATETIME_LONG = "yyyy-MM-dd HH:mm:ss.fff";
+		
+		public const string TIME = "HH:mm:ss";
+		public const string TIME_LONG = "HH:mm:ss.fff";
+		
+		public static IFormatProvider NumberFormat = CultureInfo.InvariantCulture.NumberFormat;		
+
+		public class Reader : IDisposable
+		{
+			private XmlReader reader;
+			
+			public XmlNodeType NodeType
+			{
+				get { return reader.NodeType; }
+			}
+			
+			public string Name
+			{
+				get { return reader.Name; }
+			}
+			
+			public string Value
+			{
+				get { return reader.Value; }
+			}
+			
+			
+			public Reader(string path)
+			{
+				this.reader = XmlReader.Create(path, ReaderSettings);
+			}
+			
+			public Reader(StringReader stringReader)
+			{
+				this.reader = XmlReader.Create(stringReader, ReaderSettings);
+			}
+			
+			public string GetAttributeString(string name)
+			{
+				try
+				{
+					string attributeValue = this.reader.GetAttribute(name);
+					if (String.IsNullOrEmpty(attributeValue)) return "";
+					
+					return this.reader.GetAttribute(name);
+				}
+				catch
+				{
+					
+				}
+				
+				return "";
+			}
+			
+			public DateTime GetAttributeTimestamp(string name)
+			{
+				try
+				{
+					string attributeValue = this.reader.GetAttribute(name);
+					return DateTime.ParseExact(attributeValue, XML.DATETIME_LONG, DateTimeFormatInfo.InvariantInfo);
+				}
+				catch
+				{
+					
+				}
+
+				return new DateTime(2001, 1, 1);
+			}
+			
+			public TimeSpan GetAttributeTimeSpan(string name)
+			{
+				try
+				{
+					string attributeValue = this.reader.GetAttribute(name);
+					return DateTime.ParseExact(attributeValue, XML.TIME, DateTimeFormatInfo.InvariantInfo).TimeOfDay;
+				}
+				catch
+				{
+					
+				}
+
+				return new TimeSpan(0, 0, 0);
+			}
+
+			public long GetAttributeLong(string name)
+			{
+				try
+				{
+					string attributeValue = GetAttributeString(name);
+					return long.Parse(attributeValue);
+				}
+				catch
+				{
+					
+				}
+				
+				return 0;
+			}
+			
+			public string GetValueString()
+			{
+				try
+				{
+					reader.Read();
+					//TODO: add some error checking to verify element type
+					return reader.Value;
+				}
+				catch
+				{
+					
+				}
+				
+				return "";
+			}
+			
+			public DateTime GetValueTimestamp()
+			{
+				try
+				{
+					string value = this.GetValueString();
+					return DateTime.ParseExact(value, XML.DATETIME_LONG, DateTimeFormatInfo.InvariantInfo);
+				}
+				catch
+				{
+					
+				}
+
+				return new DateTime(2001, 1, 1);
+			}
+			
+			public TimeSpan GetValueTimeSpan()
+			{
+				try
+				{
+					string value = this.GetValueString();
+					return DateTime.ParseExact(value, XML.TIME, DateTimeFormatInfo.InvariantInfo).TimeOfDay;
+				}
+				catch
+				{
+					
+				}
+
+				return new TimeSpan(0, 0, 0);
+			}
+
+			public long GetValueLong()
+			{
+				try
+				{
+					string value = this.GetValueString();
+					return long.Parse(value);
+				}
+				catch
+				{
+					
+				}
+				
+				return 0;
+			}
+			
+			public bool Read()
+			{
+				return this.reader.Read();
+			}
+			
+			public void Close()
+			{
+				this.reader.Close();
+			}
+			
+			private static XmlReaderSettings ReaderSettings
+			{
+				get
+				{
+					XmlReaderSettings settings = new XmlReaderSettings();
+					settings.CloseInput = true;
+					settings.IgnoreComments = true;
+					settings.IgnoreProcessingInstructions = true;
+					settings.IgnoreWhitespace = true;
+					return settings;
+				}
+			}
+			
+			public void Dispose()
+			{
+				this.Close();
+				
+				Dispose(true);
+				GC.SuppressFinalize(this);
+			}
+			
+			protected virtual void Dispose(bool disposing)
+			{
+				if(disposing)
+				{
+
+				}
+			}
+		}
+
+		public class Writer : IDisposable
+		{
+			private XmlWriter writer;
+			
+			public Writer(string path)
+			{
+				this.writer = XmlWriter.Create(path, WriterSettings);
+				this.writer.WriteStartDocument();
+				this.writer.WriteStartElement(sar.Tools.AssemblyInfo.Name);
+				this.writer.WriteAttributeString("version", sar.Tools.AssemblyInfo.Version);
+			}
+			
+			public Writer(StringWriter stringWriter)
+			{
+				this.writer = XmlWriter.Create(stringWriter, WriterSettings);
+				this.writer.WriteStartDocument();
+				this.WriteStartElement(sar.Tools.AssemblyInfo.Name);
+				this.WriteAttributeString("version", sar.Tools.AssemblyInfo.Version);
+			}
+			
+			public void WriteAttributeString(string name, string value)
+			{
+				if (!String.IsNullOrEmpty(value)) this.writer.WriteAttributeString(name, value);
+			}
+			
+			public void WriteAttributeString(string name, DateTime value)
+			{
+				string attributeValue = value.ToString(XML.DATETIME_LONG);
+				this.WriteAttributeString(name, attributeValue);
+			}
+
+			public void WriteAttributeString(string name, TimeSpan value)
+			{
+				string attributeValue = value.ToString();
+				this.WriteAttributeString(name, attributeValue);
+			}
+			
+			public void WriteAttributeString(string name, long value)
+			{
+				string attributeValue = value.ToString();
+				this.WriteAttributeString(name, attributeValue);
+			}
+			
+			public void WriteElement(string element, string value)
+			{
+				if (String.IsNullOrEmpty(value)) return;
+				if (String.IsNullOrEmpty(element)) return;
+				
+				this.WriteStartElement(element);
+				this.WriteValue(value);
+				this.WriteEndElement();
+			}	
+
+			public void WriteElement(string element, DateTime value)
+			{
+				string elementValue = value.ToString(XML.DATETIME_LONG);
+				this.WriteElement(element, elementValue);
+			}	
+
+			public void WriteElement(string element, TimeSpan value)
+			{
+				string elementValue = value.ToString();
+				this.WriteElement(element, elementValue);
+			}				
+
+			public void WriteElement(string element, long value)
+			{
+				string elementValue = value.ToString();
+				this.WriteElement(element, elementValue);
+			}
+			
+			public void WriteValue(string value)
+			{
+				writer.WriteValue(value);
+			}
+			
+			public void WriteStartElement(string name)
+			{
+				this.writer.WriteStartElement(name);
+			}
+
+			public void WriteEndElement()
+			{
+				this.writer.WriteEndElement();
+			}
+			
+			public void Close()
+			{
+				this.WriteEndElement();
+				this.writer.WriteEndDocument();
+				this.writer.Close();
+			}
+			
+			private static XmlWriterSettings WriterSettings
+			{
+				get
+				{
+					XmlWriterSettings settings = new XmlWriterSettings();
+					settings.CloseOutput = true;
+					settings.Encoding = Encoding.UTF8;
+					settings.Indent = true;
+					settings.IndentChars = "\t";
+					return settings;
+				}
+			}
+			
+			public void Dispose()
+			{
+				this.Close();
+				
+				Dispose(true);
+				GC.SuppressFinalize(this);
+			}
+			
+			protected virtual void Dispose(bool disposing)
+			{
+				if(disposing)
+				{
+
+				}
+			}
+		}
+	}
+}
