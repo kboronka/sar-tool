c72b48d4423d21fc71e62dd469717210f7b40751
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-11-01 20:34:58 +0000

    added ReadDINT and ReadFLOAT methods

--- sarTesting/Menu.Designer.cs
+++ sarTesting/Menu.Designer.cs
@@ -52,6 +52,7 @@ namespace sar.Testing
 			this.ConnectToSPS = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -143,11 +144,19 @@ namespace sar.Testing
 			this.textBox1.Size = new System.Drawing.Size(168, 20);
 			this.textBox1.TabIndex = 7;
 			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(288, 112);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(168, 20);
+			this.textBox2.TabIndex = 8;
+			// 
 			// Menu
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(507, 235);
+			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.ConnectToSPS);
@@ -163,6 +172,7 @@ namespace sar.Testing
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
+		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button ConnectToSPS;
