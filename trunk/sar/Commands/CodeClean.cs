21054e05e809cf8aaa785184ed5d8ba030fd5c12
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-11-18 02:19:58 +0000

    fix line vb.net continuations starting with "("

--- trunk/sar/Commands/CodeClean.cs
+++ trunk/sar/Commands/CodeClean.cs
@@ -68,7 +68,10 @@ namespace sar.Commands
 							changes += IO.SearchAndReplaceInFile(file, @"[\s]*[_]{1}[\s]*[\n\r][\s]*(')", @" $1");
 							
 							// fix the "_ )" lines
-							changes += IO.SearchAndReplaceInFile(file, @"[\s]*[_]{1}[\s]*[\n\r][\s]*(\))", @" $1");
+							changes += IO.SearchAndReplaceInFile(file, @"[\s]*[_]{1}[\s]*[\n\r][\s]*(\))", @"$1");
+
+							// fix the "_ ( lines
+							changes += IO.SearchAndReplaceInFile(file, @"[\s]*[_]{1}[\s]*[\n\r][\s]*(\()", @"$1");
 
 							// remove empty lines after "Then"
 							changes += IO.SearchAndReplaceInFile(file, @"Then\r*\n\s*\r*\n(\s*)(\S)", "Then\r\n$1$2");
