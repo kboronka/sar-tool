3676b4dbd00ad92592f2f9e94a3391a02a178288
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-11-20 02:34:15 +0000

    Tagging the 1.0.1.228 version release of the project

---
+++ tags/1.0.1.228/sarValidate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+using sar.Tools;
+
+namespace sarValidate
+{
+	class Program : sar.Base.Program
+	{
+		public static int Main(string[] args)
+		{
+			try
+			{
+				Program.LogInfo();
+				
+				try
+				{
+					CommandHub hub = new CommandHub();
+					ConsoleHelper.Start();
+					ConsoleHelper.ApplicationShortTitle();
+					hub.ProcessCommands(args);
+				}
+				catch (Exception ex)
+				{
+					ConsoleHelper.WriteException(ex);
+
+				}
+				
+				ConsoleHelper.Shutdown();
+				return ConsoleHelper.EXIT_OK;
+			}
+			catch
+			{
+				return ConsoleHelper.EXIT_ERROR;
+			}
+		}
+	}
+}
