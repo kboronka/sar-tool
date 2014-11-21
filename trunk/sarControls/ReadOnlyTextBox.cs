55d25b40b37e637b55c827a41ec12222bea6737a
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-11-21 01:00:56 +0000

    added a read-only textbox UserControl which hides the caret

---
+++ trunk/sarControls/ReadOnlyTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace sar.Controls
+{
+	public class ReadOnlyTextBox : TextBox
+	{
+		[DllImport("user32.dll")]
+		static extern bool HideCaret(IntPtr hWnd);
+
+		public ReadOnlyTextBox()
+		{
+			this.ReadOnly = true;
+			this.BackColor = Color.White;
+			this.GotFocus += TextBoxGotFocus;
+			this.Cursor = Cursors.Arrow; // mouse cursor like in other controls
+			this.Multiline = true;
+		}
+
+		private void TextBoxGotFocus(object sender, EventArgs args)
+		{
+			HideCaret(this.Handle);
+		}
+	}
+}
--- trunk/sarTesting/Menu.Designer.cs
+++ trunk/sarTesting/Menu.Designer.cs
@@ -53,6 +53,7 @@ namespace sar.Testing
 			this.button2 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.readOnlyTextBox1 = new sar.Controls.ReadOnlyTextBox();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -151,11 +152,24 @@ namespace sar.Testing
 			this.textBox2.Size = new System.Drawing.Size(168, 20);
 			this.textBox2.TabIndex = 8;
 			// 
+			// readOnlyTextBox1
+			// 
+			this.readOnlyTextBox1.BackColor = System.Drawing.Color.Yellow;
+			this.readOnlyTextBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.readOnlyTextBox1.Location = new System.Drawing.Point(8, 224);
+			this.readOnlyTextBox1.Multiline = true;
+			this.readOnlyTextBox1.Name = "readOnlyTextBox1";
+			this.readOnlyTextBox1.ReadOnly = true;
+			this.readOnlyTextBox1.Size = new System.Drawing.Size(304, 120);
+			this.readOnlyTextBox1.TabIndex = 9;
+			this.readOnlyTextBox1.Text = "line 1\r\nline 2";
+			// 
 			// Menu
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(507, 235);
+			this.ClientSize = new System.Drawing.Size(507, 382);
+			this.Controls.Add(this.readOnlyTextBox1);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.button2);
@@ -172,6 +186,7 @@ namespace sar.Testing
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
+		private sar.Controls.ReadOnlyTextBox readOnlyTextBox1;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button button2;
