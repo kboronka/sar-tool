8b98b7cc42b089a4edf6d9b27c59286654ca3ea7
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2014-11-21 01:41:11 +0000

    Tagging the 1.0.1.230 version release of the project

---
+++ tags/1.0.1.230/sarControls/ReadOnlyTextBox.cs
@@ -0,0 +1,42 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace sar.Controls
+{
+	public class ReadOnlyTextBox : TextBox
+	{
+		[DllImport("user32.dll")]
+		static extern bool HideCaret(IntPtr hWnd);
+
+		public ReadOnlyTextBox()
+		{
+			this.ReadOnly = true;
+			this.BackColor = Color.White;
+			this.GotFocus += TextBoxGotFocus;
+			this.Cursor = Cursors.Arrow; // mouse cursor like in other controls
+			this.Multiline = true;
+		}
+
+		private void TextBoxGotFocus(object sender, EventArgs args)
+		{
+			HideCaret(this.Handle);
+		}
+	}
+}
