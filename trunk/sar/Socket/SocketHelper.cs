39e174ecccd12a8e2192062c8e9ca010b9e31fbe
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2015-01-13 01:53:01 +0000

    added SocketHelper class added FindAvailablePort method

---
+++ trunk/sar/Socket/SocketHelper.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace sar.Socket
+{
+	public static class SocketHelper
+	{
+		private static int FindAvailablePort(int firstPort, int lastPort)
+		{
+			IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+			IPEndPoint[] tcpEndPoints = properties.GetActiveTcpListeners();
+			
+			List<int> usedPorts = new List<int>();
+			foreach (IPEndPoint endpoint in tcpEndPoints)
+			{
+				usedPorts.Add(endpoint.Port);
+			}
+
+			for (int port = firstPort; port < lastPort; port++)
+			{
+				if (!usedPorts.Contains(port)) return port;
+			}
+			
+			return -1;
+		}
+	}
+}
