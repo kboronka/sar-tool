23963e0df4df6f010e2446e4c2718da7b6e84f08
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2015-01-27 02:59:42 +0000

    organized paths

---
+++ sarTesting/Http/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace sar_testing.Http
+{
+	/// <summary>
+	/// Description of TestController.
+	/// </summary>
+	public class TestController
+	{
+		public TestController()
+		{
+		}
+	}
+}
