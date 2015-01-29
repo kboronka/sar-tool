d5d79d1b4ac0137468292a0ca1a2ea8f0066e954
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2015-01-29 01:22:12 +0000

    added a controller for testing ajax calls

---
+++ sar/Http/Controllers/EchoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using sar.Tools;
+
+namespace sar.Http
+{
+	// used for testing ajax calls
+	public static class echoController
+	{
+		public static HttpContent html(HttpRequest request)
+		{
+			return new HttpContent(request.Query);
+		}
+		
+		public static HttpContent json(HttpRequest request)
+		{
+			return new HttpContent("tbd-json");
+		}
+		
+		public static HttpContent xml(HttpRequest request)
+		{
+			return new HttpContent("tbd-xml");
+		}
+	}
+}
