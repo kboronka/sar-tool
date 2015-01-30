d91aeaf05d427b43131feddf7be7738951dc858b
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2015-01-30 03:31:11 +0000

    Tagging the 1.0.1.246 version release of the project

---
+++ tags/1.0.1.246/sar/Http/Controllers/EchoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using sar.Tools;
+
+namespace sar.Http
+{
+	// used for testing ajax calls
+	public static class echoController
+	{
+		public static HttpContent html(HttpRequest request)
+		{
+			return new HttpContent(request.Data, "text/html");
+		}
+		
+		public static HttpContent json(HttpRequest request)
+		{
+			try
+			{
+				Dictionary<string, string> result = new Dictionary<string, string>();
+				result.Add("request", Encoding.ASCII.GetString(request.Data));
+				result.Add("other", Guid.NewGuid().ToString());
+				
+				if (result["request"] == "error") throw new ApplicationException("error test");
+				
+				return new HttpContent(result.ToJSON());
+			}
+			catch (Exception ex)
+			{
+				return new HttpErrorContent(ex);
+			}
+		}
+	}
+}
