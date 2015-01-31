0d8d7f0114ab1e04675030b8660caa3a70dc8b79
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2015-01-31 02:56:43 +0000

    Tagging the 1.0.1.250 version release of the project

---
+++ tags/1.0.1.250/sar/Commands/VboxManage.cs
@@ -0,0 +1,67 @@
+/* Copyright (C) 2015 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using sar.Tools;
+using sar.Base;
+
+namespace sar.Commands
+{
+	public class VboxManage : Command
+	{
+		public VboxManage(Base.CommandHub parent) : base(parent, "Vbox Manage Tool",
+		                           new List<string> { "vbox.manage", "vb.manage" },
+		                           @"-vbox.manage [arg1] [arg2] [arg3]",
+		                           new List<string> { "-vbox.manage modifyhd \"WinXP-disk1.vdi\" --resize 3000" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length < 2)
+			{
+				throw new ArgumentException("too few arguments");
+			}
+					
+			string exePath = IO.FindApplication("VBoxManage.exe");
+			
+			string arguments = "";
+			for (int i = 1; i < args.Length; i++)
+			{
+				arguments += " " + args[i];
+			}
+
+			string output;
+			int exitcode = ConsoleHelper.Run(exePath, arguments, out output);
+				
+			if (exitcode != 0)
+			{
+				ConsoleHelper.WriteLine("Command Failed", ConsoleColor.DarkYellow);
+				ConsoleHelper.WriteLine(output, ConsoleColor.DarkCyan);
+				ConsoleHelper.WriteLine("exit code: " + exitcode.ToString());
+				return exitcode;
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("Command Successfully Completed", ConsoleColor.DarkYellow);
+				return ConsoleHelper.EXIT_OK;
+			}
+		}
+	}
+}
---
+++ tags/1.0.1.250/sar/Program.cs
@@ -0,0 +1,92 @@
+/* Copyright (C) 2015 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+using sar.Commands;
+using sar.Tools;
+
+
+// binary download: https://sar-tool.googlecode.com/svn/trunk/release/sar.exe
+// release download: https://sar-tool.googlecode.com/svn/tags/
+
+namespace sar
+{
+	public class Program
+	{
+		public static int Main(string[] args)
+		{
+			try
+			{
+				
+				#if DEBUG
+				/*
+				args = new string[] { "f.rd", @"C:\Users\kboronka\Documents\Virtual Machines\caches", @"/pause" };
+				args = new string[] { "rdp", "192.168.171.208", @"username", "password", @"/pause" };
+				args = new string[] { "ip.set", "LAN", "dhcp", @"/debug", @"/pause", @"/admin" };
+				args = new string[] { "ip.set", "LAN", "192.168.14.111", "255.255.255.0", @"/debug", @"/pause", @"/admin" };
+				args = new string[] { "ip.config", @"/pause" };
+				*/
+				#endif
+				
+				CommandHub hub = new CommandHub();
+				ConsoleHelper.Start();	
+				if (args.Length == 0) ConsoleHelper.ApplicationTitle();
+				int exitCode = hub.ProcessCommands(args);
+				
+				ConsoleHelper.Shutdown();
+				return exitCode;
+			}
+			catch (Exception ex)
+			{
+				ConsoleHelper.WriteException(ex);
+				ConsoleHelper.Shutdown();
+				return ConsoleHelper.EXIT_ERROR;
+			}
+		}
+		
+		#region loging functions
+		
+		public static void Log(Exception ex)
+		{
+			try
+			{
+				sar.Base.Program.Log(ex);
+			}
+			catch
+			{
+				
+			}
+		}
+		
+		public static void Log(string message)
+		{
+			try
+			{
+				sar.Base.Program.Log(message);
+			}
+			catch
+			{
+
+			}
+		}
+		
+		#endregion
+	}
+}
