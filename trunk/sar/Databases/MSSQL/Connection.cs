3d5eb9a80d424559d10d4f6d669beef6875555eb
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2015-02-19 01:50:05 +0000

    added connection string helper

---
+++ trunk/sar/Databases/MSSQL/Connection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace sar.Databases.MSSQL
+{
+	public class ConnectionString
+	{
+		#region members
+
+		private string server;
+		private string database;
+		private string username;
+		private string password;
+
+		#endregion
+
+		#region constructor
+
+		public ConnectionString(string server, string database, string username, string password)
+		{
+			this.server = server;
+			this.database = database;
+			this.username = username;
+			this.password = password;
+		}
+
+		#endregion
+
+		#region methods
+
+		public override string ToString()
+		{
+			string connectionString = "";
+			connectionString += "Data Source=" + this.server + ";";
+			connectionString += "Initial Catalog=" + this.database + ";";
+			connectionString += "User Id=" + this.username + ";";
+			connectionString += "Password=" + this.password + ";";
+			connectionString += "Application Name=" + sar.Tools.AssemblyInfo.Name + ";";
+			
+			return connectionString;
+		}
+
+		#endregion
+	}
+}
