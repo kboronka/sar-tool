535778f0eb908d119d0ec0578e1a0f0e27c88a9e
kboronka <kboronka@e0181cce-766e-d700-03fe-165ee013faab>
2015-02-26 03:02:21 +0000

    Tagging the 1.0.1.266 version release of the project

---
+++ tags/1.0.1.266/sar/Tools/ExceptionHandler.cs
@@ -0,0 +1,42 @@
+/* Copyright (C) 2015 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace sar.Tools
+{
+	public partial class ExceptionHandler : Form
+	{
+		public ExceptionHandler(Exception ex)
+		{
+			InitializeComponent();
+			this.Text = ex.GetType().ToString();
+			this.details.Text = ex.Message;
+			this.details.Text += Environment.NewLine;
+			this.details.Text += ExceptionHelper.GetStackTrace(ex);
+		}
+		
+		public static void Display(Exception ex)
+		{
+			// temporary
+			ExceptionHandler form = new ExceptionHandler(ExceptionHelper.GetInner(ex));
+			form.ShowDialog();
+		}
+	}
+}
