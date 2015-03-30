c641a9201cafe65034750261cfe35623bd79e3d4
Kevin Boronka <kboronka@gmail.com>
2015-03-30 17:01:10 -0400

    added a datetime to string extension method

---
+++ sar/Databases/DatabaseHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sar.Tools
+{
+	public static class DatabaseHelper
+	{
+		public static string ToDBString(this DateTime value)
+		{
+			return "'" + value.ToString(FileLogger.ISO8601_TIMESTAMP) + "'";
+		}
+	}
+}
