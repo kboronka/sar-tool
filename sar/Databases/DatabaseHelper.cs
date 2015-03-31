c5d5511d747adbe8967b10bb36a685d271cd4a1f
Kevin Boronka <kboronka@gmail.com>
2015-03-31 13:37:36 -0400

    sanity of datetime values

--- sar/Databases/DatabaseHelper.cs
+++ sar/Databases/DatabaseHelper.cs
@@ -6,7 +6,18 @@ namespace sar.Tools
 	{
 		public static string ToDBString(this DateTime value)
 		{
-			return "'" + value.ToString(FileLogger.ISO8601_TIMESTAMP) + "'";
+			if (value == DateTime.MaxValue)
+			{
+				return "'2990-01-01T00:00:00.000'";
+			}
+			else if (value == DateTime.MinValue)
+			{
+				return "'1990-01-01T00:00:00.000'";
+			}
+			else
+			{
+				return "'" + value.ToString(FileLogger.ISO8601_TIMESTAMP) + "'";
+			}
 		}
 	}
 }
