ddab5b53f7e8d45a9a28f92e8eeaa0fe5ef1d273
Kevin Boronka <kboronka@gmail.com>
2015-05-15 11:20:46 -0400

    added some formating options to the WKHTMLTOPDF call

--- sar/Http/HttpContent.cs
+++ sar/Http/HttpContent.cs
@@ -199,9 +199,10 @@ namespace sar.Http
 			if (string.IsNullOrEmpty(PdfWriter)) throw new ApplicationException("PDF output is not supported");
 			
 			string tempfile = ApplicationInfo.DataDirectory + Guid.NewGuid().ToString() + ".pdf";
+			const string options = "--page-size A4 --viewport-size 1280x1024 --margin-top 3 --margin-bottom 3 --margin-left 3 --margin-right 3";
 			string output = "";
 			
-			int exitCode = ConsoleHelper.Run(PdfWriter, url.QuoteDouble()+ " " + tempfile.QuoteDouble(), out output);
+			int exitCode = ConsoleHelper.Run(PdfWriter, options + " " + url.QuoteDouble() + " " + tempfile.QuoteDouble(), out output);
 			
 			
 			HttpContent result = HttpContent.Read(tempfile);
