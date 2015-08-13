1f98b2e284dd7db44271d19be9ba5677da66f015
Kevin Boronka <kboronka@gmail.com>
2015-08-13 17:36:33 -0400

    write result to console

--- sar/Commands/Bower.cs
+++ sar/Commands/Bower.cs
@@ -1,4 +1,19 @@
-﻿using System;
+/* Copyright (C) 2015 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -27,7 +42,10 @@ namespace sar.Commands
 			if (!File.Exists(bower)) throw new ApplicationException("Bower not found");
 			
 			//ConsoleHelper.Run(nodejs, bower + " install");
-			ConsoleHelper.Run(nodejs, bower + " update");
+			string output;
+			ConsoleHelper.Run(nodejs, bower + " update", out output);
+			
+			ConsoleHelper.WriteLine(output, ConsoleColor.White);
 			
 			ConsoleHelper.WriteLine("Bower update was successfully completed", ConsoleColor.DarkYellow);
 			return ConsoleHelper.EXIT_OK;
