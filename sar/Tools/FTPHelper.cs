4e6a920cca4503f74950ff74441b0e42e0edd081
Kevin Boronka <kboronka@gmail.com>
2015-10-19 15:55:05 -0400

    cleanup

--- sar/Tools/FTPHelper.cs
+++ sar/Tools/FTPHelper.cs
@@ -1,4 +1,19 @@
-﻿using System;
+/* Copyright (C) 2015 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -9,7 +24,7 @@ namespace sar.Tools
 	{
 		public static List<string> GetFileList(string ip)
 		{
-			var downloadFiles = new List<string>();
+			var files = new List<string>();
 			
 			var reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + ip + "/"));
 			reqFTP.UseBinary = true;
@@ -26,12 +41,12 @@ namespace sar.Tools
 			while (line != null)
 			{
 				line.Replace("\n", "");
-				downloadFiles.Add(line);
+				files.Add(line);
 				
 				line = reader.ReadLine();
 			}
 			
-			return downloadFiles;
+			return files;
 		}
 
 		public static byte[] DownloadBytes(string ip, string file)
