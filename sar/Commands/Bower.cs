3c144d04e8cbaaafa8e36741d4891c4b539eb118
Kevin Boronka <kboronka@gmail.com>
2015-11-11 17:11:56 -0500

    clean cache

--- sar/Commands/Bower.cs
+++ sar/Commands/Bower.cs
@@ -43,7 +43,7 @@ namespace sar.Commands
 			
 			if (!File.Exists(bower)) throw new ApplicationException("Bower not found");
 			
-			//ConsoleHelper.Run(nodejs, bower + " cache clean");
+			ConsoleHelper.Run(nodejs, bower + " cache clean");
 			ConsoleHelper.Run(nodejs, bower + " install");
 			ConsoleHelper.Run(nodejs, bower + "-force update");
 						
