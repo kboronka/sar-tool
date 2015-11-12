e50b6ae2180836e95b1b25d1c287edb991e383ce
Kevin Boronka <kboronka@gmail.com>
2015-11-12 17:08:07 -0500

    added an interval object

---
+++ sar/Control/Interval.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace sar.Control
+{
+	public class Interval
+	{
+		private static Stopwatch time;
+		
+		private static long Time
+		{
+			get
+			{
+				
+				if (time == null)
+				{
+					time = new Stopwatch();
+					time.Start();
+				}
+				
+				return time.ElapsedMilliseconds;
+			}
+		}
+		
+		private long interval;
+		private long lastTrigger;
+		
+		public Interval(long interval)
+		{
+			this.interval = interval;
+			this.lastTrigger = Interval.Time;
+		}
+		
+		public bool Ready
+		{
+			get
+			{
+				if (Time - lastTrigger > interval)
+				{
+					this.lastTrigger += interval;
+					return true;
+				}
+				
+				return false;
+			}
+		}
+	}
+}
