c24fb480004642c93a6aca6eed2e765e48615f76
Kevin Boronka <kboronka@gmail.com>
2016-10-27 09:41:17 -0400

    added a winforms helper class

---
+++ sar/Tools/WinFormsHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace sar.Tools
+{
+	public static class WinFormsHelper
+	{
+		// source: http://stackoverflow.com/a/29497681/259712
+		public delegate void InvokeIfRequiredDelegate<T>(T obj) where T : ISynchronizeInvoke;
+
+		public static void InvokeIfRequired<T>(this T obj, InvokeIfRequiredDelegate<T> action) where T : ISynchronizeInvoke
+		{
+			if (obj.InvokeRequired)
+			{
+				obj.Invoke(action, new object[] { obj });
+			}
+			else
+			{
+				action(obj);
+			}
+		}
+	}
+}
