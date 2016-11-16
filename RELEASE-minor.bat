b58625d21a87daeff3d58aa3a2b9f36fd4232cda
Kevin Boronka <kboronka@gmail.com>
2016-11-16 18:13:29 -0500

    new binaries v1.1.0.453

--- RELEASE-minor.bat
+++ RELEASE-minor.bat
@@ -56,7 +56,6 @@
 	svn commit -m "new binaries v%VERSION%"
 	%ZIP% "sar %VERSION%.zip" .\release\*.*
 	echo %ZIP% "sar %VERSION%.zip" .\release\*.*
-	pause
 	svn update
 
 	%SAR% -f.bsd \sar\*.cs "Kevin Boronka"
