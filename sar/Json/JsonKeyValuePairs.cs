6b0d5258283e2fab92463ddbeb9a15a149567f73
Kevin Boronka <kboronka@gmail.com>
2017-04-04 10:12:41 -0400

    added JsonKeyValuePairs class

---
+++ sar/Json/JsonKeyValuePairs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sar.Json
+{
+	/// <summary>
+	/// Dictionary extension object.
+	/// </summary>
+	public class JsonKeyValuePairs : Dictionary<string, object>
+	{
+		
+	}
+}
