340843505aaf09b61fae9db4daaccb1bfc15e83c
Kevin Boronka <kboronka@gmail.com>
2017-08-15 11:14:57 -0400

    added finite state machine helpers

---
+++ sar/FSM/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace sar.FSM
+{
+	/// <summary>
+	/// Description of Command.
+	/// </summary>
+	public class Command
+	{
+		public Enum CommandSignal { get; private set; }
+		public Object Parameters { get; private set; }
+		
+		public Command(Enum command, Object parameters)
+		{
+			this.CommandSignal = command;
+			this.Parameters = parameters;
+		}
+		
+		public Command(Enum command)
+			: this(command, null)
+		{
+			
+		}
+	}
+}
---
+++ sar/FSM/CommandQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sar.FSM
+{
+	/// <summary>
+	/// Description of CommandQueue.
+	/// </summary>
+	public class CommandQueue
+	{
+		private readonly List<Command> commands;
+		
+		public bool Available { get { return commands.Count > 0; } }
+		
+		public CommandQueue()
+		{
+			commands = new List<Command>();
+		}
+		
+		public void QueueCommand(Enum command)
+		{
+			commands.Add(new Command(command));
+		}
+		
+		public void QueueCommand(Enum command, object paramerter)
+		{
+			commands.Add(new Command(command, paramerter));
+		}
+
+		private Command DequeueCommand()
+		{
+			if (commands.Count == 0)
+			{
+				return null;
+			}
+			
+			var currentCommand = commands[commands.Count - 1];
+			commands.RemoveAt(commands.Count - 1);
+			
+			return currentCommand;
+		}
+	}
+}
