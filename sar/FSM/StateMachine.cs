c09e7d5df3ab221df821d76f652b8c6bf917ce08
Kevin Boronka <kboronka@gmail.com>
2017-08-15 11:27:26 -0400

    added a StateMachine base class

---
+++ sar/FSM/StateMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace sar.FSM
+{
+	/// <summary>
+	/// Description of StateMachine.
+	/// </summary>
+	public abstract class StateMachine
+	{
+		public CommandQueue CommandQueue;
+
+		protected bool loopStopRequested;
+		protected bool loopStopped;
+		
+		protected StateMachine()
+		{
+			CommandQueue = new CommandQueue();
+			loopStopped = false;
+			loopStopRequested = false;
+		}
+		
+		protected abstract void Start();
+		protected abstract void Stop();		
+	}
+}
