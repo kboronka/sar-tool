be281097dc3b5d7a1e85e6d71032e7db19e2d6ac
Kevin Boronka <kboronka@gmail.com>
2017-10-06 17:28:29 -0400

    new binaries v1.1.0.539

--- sar/Program.cs
+++ sar/Program.cs
@@ -60,10 +60,9 @@ namespace sar
 				args = new string[] { "svn.GetNewAssemblyVersion", @"https://github.com/kboronka/sar-tool/trunk/sar/Properties/AssemblyInfo.cs", @"/pause" };
 				args = new string[] { "dotNetVersions", @"/pause" };
 				args = new string[] { "r", "test.cs", @"break;\r\n(.*)case", @"break;\n\n\n$1case", @"/pause" };
+				args = new string[] { "c.c", @"C:\Users\kboronka\Desktop\Manual", @"/pause" };
 				 */
 				
-				//args = new string[] { "c.c", @"C:\Users\kboronka\Desktop\manual-owms", @"/pause" };
-				args = new string[] { "c.c", @"C:\Users\kboronka\Desktop\Manual", @"/pause" };
 				#endif
 				
 				var hub = new CommandHub();
